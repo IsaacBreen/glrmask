e05829a5daab3c9a17142b11f1618167d8e36356
IsaacBreen <mail@isaacbreen.com>
2025-04-10 17:49:42 +0800

    simplify, remove stuff

--- src/constraint.rs
+++ src/constraint.rs
@@ -1,17 +1,17 @@
 use crate::finite_automata::{GroupID, Regex};
-use crate::glr::parser::{GLRParser, InsertWith, ParseState, ParseStateKey};
+use crate::glr::parser::{GLRParser, InsertWith, ParseState};
 use crate::glr::table::StateID;
-use crate::glr::table::TerminalID;
 use crate::trie::TrieNode;
 use bimap::BiBTreeMap;
-use bitvec::bitvec;
 use bitvec::prelude::BitVec;
 use bitvec::prelude::*;
 use std::collections::{BTreeMap, BTreeSet};
-use std::sync::{Arc, Mutex};
 
 
 pub type TokenID = usize;
+
+type LLMToken = Vec<u8>;
+type LLMTokenMap = BiBTreeMap<Vec<u8>, LLMTokenID>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct LLMTokenID(pub usize);
@@ -27,26 +27,9 @@ pub struct ExecuteResult {
     pub new_state: Option<usize>,
 }
 
-// TODO: get rid of this trait. Just implement it directly on the Tokenizer struct.
-/// Trait defining the tokenizer behavior.
-pub trait Tokenizer: Sized {
-    /// Returns the initial state ID.
-    fn initial_state_id(&self) -> usize;
-
-    /// Executes the tokenizer on the given text starting from the specified state.
-    /// Returns all possible next tokens (**not** a sequence of tokens).
-    fn execute_from_state(&self, text: &[u8], state: usize) -> ExecuteResult;
-
-    /// Returns the list of tokens accessible from the given state.
-    fn tokens_accessible_from_state(&self, state: usize) -> Vec<TokenID>;
-
-    /// Returns the maximum state ID in the DFA.
-    fn max_state(&self) -> usize;
-}
-
 /// Precomputes a map from state -> token sequence -> LLM token -> state.
 pub fn precompute<'a>(
-    tokenizer: &impl Tokenizer,
+    tokenizer: Regex,
     llm_token_map: &BiBTreeMap<Vec<u8>, LLMTokenID>,
     eof_llm_token_id: LLMTokenID,
     max_llm_token_id: usize,
@@ -54,7 +37,7 @@ pub fn precompute<'a>(
     todo!()
 }
 
-impl Tokenizer for Regex {
+impl Regex {
     fn initial_state_id(&self) -> usize {
         0
     }
@@ -83,46 +66,32 @@ impl Tokenizer for Regex {
     }
 }
 
-pub fn print_precomputed(precomputed: &BTreeMap<StateID, TrieNode<(), TokenID, (BTreeMap<LLMTokenID, Option<StateID>>, BTreeMap<TokenID, BitVec>, Option<BitVec>)>>) {
-    todo!()
-}
-
-type LLMToken = Vec<u8>;
-type LLMTokenMap = BiBTreeMap<Vec<u8>, LLMTokenID>;
-
 #[derive(Debug, Clone)]
-pub struct GrammarConstraint<T: Tokenizer> {
-    pub(crate) tokenizer: T,
+pub struct GrammarConstraint {
+    pub(crate) tokenizer: Regex,
     pub(crate) parser: GLRParser,
     pub precomputed: BTreeMap<StateID, TrieNode<(), TokenID, (BTreeMap<LLMTokenID, Option<StateID>>, BTreeMap<TokenID, BitVec>, Option<BitVec>)>>,
     pub(crate) max_llm_token_id: usize,
 }
 
 #[derive(Debug, Clone)]
-pub struct GrammarConstraintState<T: Tokenizer> {
-    parent: GrammarConstraint<T>,
+pub struct GrammarConstraintState {
+    parent: GrammarConstraint,
     states: Vec<(ParseState, BTreeSet<StateID>)>,
 }
 
-impl<T: Tokenizer> GrammarConstraint<T> {
+impl GrammarConstraint {
     pub fn new(
-        tokenizer: T,
+        tokenizer: Regex,
         parser: GLRParser,
         llm_tokens: LLMTokenMap,
         eof_llm_token_id: usize,
         max_llm_token_id: usize
     ) -> Self {
-        let mut precomputed = precompute(&tokenizer, &llm_tokens, LLMTokenID(eof_llm_token_id), max_llm_token_id);
-
-        Self {
-            tokenizer,
-            parser,
-            precomputed,
-            max_llm_token_id,
-        }
+        todo!()
     }
 
-    pub fn init(self) -> GrammarConstraintState<T> {
+    pub fn init(self) -> GrammarConstraintState {
         let parser_initial_state = self.parser.init_parse_state();
         let tokenizer_initial_state_id = StateID(self.tokenizer.initial_state_id());
 
@@ -133,115 +102,13 @@ impl<T: Tokenizer> GrammarConstraint<T> {
     }
 }
 
-impl<'a, T: Tokenizer> GrammarConstraintState<T> {
+impl<'a> GrammarConstraintState {
     pub fn get_mask(&self) -> BitVec {
-        let mut result = bitvec![0; self.parent.max_llm_token_id + 1];
-
-        let mut initial_nodes_and_values = Vec::new();
-
-        for (parse_state, tokenizer_state_ids) in &self.states {
-            for tokenizer_state in tokenizer_state_ids {
-                let token_sequence_map = &self.parent.precomputed[tokenizer_state];
-                let active_tokens = bitvec![1; self.parent.max_llm_token_id + 1];
-                initial_nodes_and_values.push((Arc::new(Mutex::new(token_sequence_map.clone())), (vec![parse_state.clone()], active_tokens)));
-            }
-        }
-
-        TrieNode::special_map(
-            initial_nodes_and_values,
-            |(current_parse_states, active_tokens), token_id, token_gate, _dst_node| {
-                let mut glr_parse_state = self.parent.parser.init_glr_parser_from_parse_states(current_parse_states.clone());
-                glr_parse_state.step(TerminalID(*token_id));
-                (glr_parse_state.active_states, active_tokens.clone())
-            },
-            |values| {
-                let mut all_parse_states = Vec::new();
-                let mut all_active_tokens = bitvec![0; self.parent.max_llm_token_id + 1];
-                for (parse_states, active_tokens) in values {
-                    all_parse_states.extend(parse_states);
-                    all_active_tokens |= active_tokens;
-                }
-                let mut new_glr_parse_state = self.parent.parser.init_glr_parser_from_parse_states(all_parse_states);
-                new_glr_parse_state.merge_active_states();
-                (new_glr_parse_state.active_states, all_active_tokens)
-
-            },
-            |(_, bitsets, maybe_clean_end_bitset), (current_parse_states, active_tokens)| {
-                let mut glr_parse_state = self.parent.parser.init_glr_parser_from_parse_states(current_parse_states.clone());
-                if glr_parse_state.is_ok() {
-                    for (possible_next_grammar_token, bitset) in bitsets {
-                        let mut new_glr_parse_state = glr_parse_state.clone();
-                        new_glr_parse_state.step(TerminalID(*possible_next_grammar_token));
-
-                        if new_glr_parse_state.is_ok() {
-                            result |= bitset;
-                        }
-                    }
-                    if let Some(bitset) = maybe_clean_end_bitset {
-                        result |= bitset;
-                    }
-                };
-                !active_tokens.is_empty()
-            },
-        );
-        result
+        todo!()
     }
 
     pub fn commit(&mut self, llm_token_id: LLMTokenID) {
-        let mut new_states: BTreeMap<(ParseStateKey, BTreeSet<StateID>), ParseState> = BTreeMap::new();
-        let mut initial_nodes_and_values = Vec::new();
-        for (parse_state, tokenizer_state_ids) in &self.states {
-            for tokenizer_state_id in tokenizer_state_ids {
-                let token_sequence_map = &self.parent.precomputed[tokenizer_state_id];
-                let active_tokens = bitvec![1; self.parent.max_llm_token_id + 1];
-                initial_nodes_and_values.push((Arc::new(Mutex::new(token_sequence_map.clone())), (vec![parse_state.clone()], active_tokens)));
-            }
-        }
-
-        // todo: should be able to remove active tokens from this, not relevant.
-
-        // todo: should be able to do the below loop more efficiently by optimising the precomputed
-        //  stuff for earlier llm token lookup
-        TrieNode::special_map(
-            // todo: it's messy that we need to access the value in dst_node here.
-            initial_nodes_and_values,
-            |(current_parse_states, active_tokens), token_id, token_gate, _dst_node| {
-                // todo: this is introducing redundancy... ?
-                let mut glr_parse_state = self.parent.parser.init_glr_parser_from_parse_states(current_parse_states.clone());
-                glr_parse_state.step(TerminalID(*token_id));
-                (glr_parse_state.active_states, active_tokens.clone())
-            },
-            |values| {
-                let mut all_parse_states = Vec::new();
-                let mut all_active_tokens = bitvec![0; self.parent.max_llm_token_id + 1];
-                for (parse_states, active_tokens) in values {
-                    all_parse_states.extend(parse_states);
-                    all_active_tokens |= active_tokens;
-                }
-                let mut new_glr_parse_state = self.parent.parser.init_glr_parser_from_parse_states(all_parse_states);
-                new_glr_parse_state.merge_active_states();
-                (new_glr_parse_state.active_states, all_active_tokens)
-            },
-            |(llm_token_id_to_state_id, _, _), (current_parse_states, active_tokens)| {
-                let mut new_glr_parse_state = self.parent.parser.init_glr_parser_from_parse_states(current_parse_states.clone());
-                if let Some(info) = llm_token_id_to_state_id.get(&llm_token_id) {
-                    for active_parse_state in new_glr_parse_state.active_states {
-                        new_states.insert_with(
-                            (active_parse_state.key(), BTreeSet::from([info.unwrap_or(StateID(0))])),
-                            active_parse_state,
-                            |old, new| {
-                                old.merge(new);
-                            },
-                        );
-                    }
-                };
-                true
-            },
-        );
-
-        self.states = new_states.into_iter().map(|((_key, tokenizer_state_ids), parse_state)| {
-            (parse_state, tokenizer_state_ids)
-        }).collect();
+        todo!()
     }
 
     pub fn commit_many(&mut self, llm_token_ids: &[LLMTokenID]) {
@@ -328,6 +195,6 @@ mod tests {
 
         // Run precompute
         let max_llm_token_id = llm_tokens.len() + 1;
-        let result = precompute(&tokenizer, &llm_token_map, LLMTokenID(max_llm_token_id), max_llm_token_id);
+        let result = precompute(tokenizer, &llm_token_map, LLMTokenID(max_llm_token_id), max_llm_token_id);
     }
 }
