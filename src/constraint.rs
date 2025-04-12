df3563a42a72780ebec5e7ed3cb60c578858bdcd
IsaacBreen <mail@isaacbreen.com>
2025-04-12 16:28:09 +0800

    fk

--- src/constraint.rs
+++ src/constraint.rs
@@ -7,17 +7,28 @@ use bitvec::prelude::BitVec;
 use bitvec::prelude::*;
 use std::collections::{BTreeMap, BTreeSet};
 use keyed_priority_queue::KeyedPriorityQueue;
+use crate::managed_glr_parser::ManagedGLRParserState;
 use crate::types::{TerminalID as GrammarTokenID};
 
 pub type LLMTokenBV = BitVec;
 pub type GrammarTokenBV = BitVec;
 
+#[derive(Debug, Clone)]
+pub struct PrecomputedFinalizer {
+    pub(crate) compatible_grammar_tokens: GrammarTokenBV,
+    pub(crate) compatible_llm_tokens: LLMTokenBV,
+    pub(crate) tokenizer_state_id: TokenizerStateID,
+}
+
+#[derive(Debug, Clone)]
+pub struct PrecomputedNodeContents {
+    pub(crate) finalizers: Vec<PrecomputedFinalizer>,
+    pub(crate) relevant_llm_token_ids: LLMTokenBV,
+}
+
 type Precomputed = BTreeMap<
     TokenizerStateID,
-    Trie<
-        GrammarTokenID,
-        Vec<(GrammarTokenBV, LLMTokenBV, TokenizerStateID)>,
-    >,
+    Trie<GrammarTokenID, PrecomputedNodeContents>,
 >;
 
 #[derive(Debug, Clone)]
@@ -32,7 +43,7 @@ pub struct GrammarConstraint {
 #[derive(Debug, Clone)]
 pub struct GrammarConstraintState<'a> {
     pub(crate) parent: &'a GrammarConstraint,
-    pub(crate) state: GLRParserState<'a>,
+    pub(crate) state: ManagedGLRParserState<'a>,
 }
 
 impl GrammarConstraint {
@@ -63,7 +74,7 @@ impl GrammarConstraint {
 
 
     pub fn init(&self) -> GrammarConstraintState<'_> {
-        let glr_parser_initial_state = self.parser.init_glr_parser();
+        let glr_parser_initial_state = self.parser.init_managed_glr_parser();
         let tokenizer_initial_state_id = self.tokenizer.initial_state_id();
 
         GrammarConstraintState {
@@ -79,10 +90,19 @@ impl GrammarConstraintState<'_> {
     }
 
     pub fn commit(&mut self, llm_token_id: LLMTokenID) {
-        todo!()
+        let mut next_node: Trie<GrammarTokenID, BTreeSet<TokenizerStateID>> = Trie::new(BTreeSet::new());
+        let mut grammar_token_trie_roots: BTreeMap<TokenizerStateID, Trie<GrammarTokenID, BTreeSet<TokenizerStateID>>> = BTreeMap::new();
+        let mut tokenizer_state_ids: BTreeSet<TokenizerStateID> = BTreeSet::new();
+        for parse_state in &self.state.active_states {
+            tokenizer_state_ids.extend(parse_state.tokenizer_state_ids.iter().cloned());
+        }
+        let token = self.parent.llm_token_map.get_by_right(&llm_token_id).unwrap();
+
     }
 
     pub fn commit_many(&mut self, llm_token_ids: &[LLMTokenID]) {
-        todo!()
+        for &llm_token_id in llm_token_ids {
+            self.commit(llm_token_id);
+        }
     }
 }
