b7dd184db075c9a1549487e91c0cc415ba1437c5
IsaacBreen <mail@isaacbreen.com>
2025-05-01 18:18:05 +0800

    uh wtf

--- examples/python/python_grammar.py
+++ examples/python/python_grammar.py
@@ -263,6 +263,7 @@ if __name__ == "__main__":
 #     for token_id in tokens:
 #         grammar_constraint_state.commit(token_id)
     mask = grammar_constraint_state.get_mask()
+    print("Got mask")
     print(f"Mask: {mask}")
     mask_ids = np.where(mask)[0].tolist()
     mask_token_ids = [tokenizer.convert_ids_to_tokens(id) for id in mask_ids]
