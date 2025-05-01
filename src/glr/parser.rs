9834b188eae98f3121b5c932ccb6111a625d9f1e
IsaacBreen <mail@isaacbreen.com>
2025-05-01 17:19:22 +0800

    ok

--- src/glr/parser.rs
+++ src/glr/parser.rs
@@ -242,7 +242,7 @@ impl<'a, T: MergeAndIntersect> GLRParserState<'a, T> {
     }
 
     pub fn step(&mut self, token_id: TerminalID) {
-        crate::debug!(3, "Stepping with token {:?}", token_id);
+        crate::debug!(3, "Stepping with token {:?}, {} active states", token_id, self.active_states.len());
         let mut next_active_states = Vec::new();
         // This will store states where the current token_id leads to no action.
         let mut current_action_not_found_states = Vec::new();
