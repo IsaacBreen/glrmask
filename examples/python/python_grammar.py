f1dc0f7facda2efef3ecb9a04f039031721bbbf4
IsaacBreen <mail@isaacbreen.com>
2025-05-02 13:30:21 +0800

    LMAO

--- examples/python/python_grammar.py
+++ examples/python/python_grammar.py
@@ -244,8 +244,10 @@ if __name__ == "__main__":
     allowed_chars = set("_")
     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if not any(c not in allowed_chars for c in k)}
 
-    # Set the vocabto just "__"
+    # Set the vocab to just underscores
     tokenizer_vocab = {"_": 0, "__": 1}
+
+    tokenizer_vocab = {"a": 0, "aa": 1}
 
     llm_token_to_id = {token.replace("Ġ", " ").encode(): i for token, i in tokenizer_vocab.items()}
     llm_tokens = list(tokenizer_vocab.keys()) # Use all tokens
