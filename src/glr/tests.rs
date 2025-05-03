ff9031af598b642ae0f161b38b89ac18961e1f7d
IsaacBreen <mail@isaacbreen.com>
2025-05-03 18:08:29 +0800

    don't ingore

--- src/glr/tests.rs
+++ src/glr/tests.rs
@@ -489,7 +489,6 @@ fn test_epsilon_rules_ambiguity() {
     assert!(state.is_ok(), "GLR parser should accept ambiguous input involving epsilon rules");
 }
 
-#[ignore]
 #[test]
 fn test_highly_ambiguous_potentially_slow() {
     // Grammar: S -> S S | a
