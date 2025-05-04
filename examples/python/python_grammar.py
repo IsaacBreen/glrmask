88962049e39b2851e92ec24350cbdf56213e9a08
IsaacBreen <mail@isaacbreen.com>
2025-05-04 11:18:41 +0800

    ok

--- examples/python/python_grammar.py
+++ examples/python/python_grammar.py
@@ -159,7 +159,7 @@ def pegen_to_sep1_grammar(grammar: pegen.grammar.Grammar) -> PyGrammar:
     # Make sure the start production is first
 #     exprs.append(("start'''", ge.ref("file")))
     # TODO: delete this
-    # Add a rule for "hello=world" to the start rule
+    # Add a rule for "hello=world$" to the start rule
     exprs.append(("start'''", ge.choice([ge.ref("file"), ge.sequence([ge.regex(eat("hello")), ge.regex(eat("=")), ge.regex(eat("world")), ge.regex(eat("$"))])])))
 
     for rule in grammar.rules.values():
