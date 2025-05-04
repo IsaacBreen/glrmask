c3b0f804b4266dd3badb8f0b9b15ddafeac27c07
IsaacBreen <mail@isaacbreen.com>
2025-05-04 15:46:40 +0800

    hm

--- examples/python/python_grammar.py
+++ examples/python/python_grammar.py
@@ -156,7 +156,7 @@ def pegen_to_sep1_grammar(grammar: pegen.grammar.Grammar) -> PyGrammar:
     exprs: list[tuple[str, Any]] = []
 
     # Make sure the start production is first
-    exprs.append(("start'''", ge.ref("file")))
+#     exprs.append(("start'''", ge.ref("file")))
 #     # TODO: delete this
 #     # Add a rule for "hello=world$" to the start rule
 #     exprs.append(("start'''", ge.choice([ge.ref("file"), ge.sequence([ge.regex(eat("hello")), ge.regex(eat("=")), ge.regex(eat("world")), ge.regex(eat("$"))])])))
@@ -164,9 +164,9 @@ def pegen_to_sep1_grammar(grammar: pegen.grammar.Grammar) -> PyGrammar:
 #     exprs.append(("file", ge.sequence([ge.regex(eat("fk")), ge.regex(eat("ing"))])))
 
 #     # TODO: delete this
-#     exprs.append(("start'''", ge.choice([ge.ref("file"), ge.sequence([ge.regex(eat("hello")), ge.regex(eat("=")), ge.regex(eat("world")), ge.regex(eat("$"))])])))
-#     SOFT_KEYWORDS = ["a", "b", "c", "d", "e", "f", "g", "h"]
-#     exprs.append(("file", ge.choice([ge.regex(eat(soft_keyword)) for soft_keyword in SOFT_KEYWORDS])))
+    exprs.append(("start'''", ge.choice([ge.ref("file"), ge.sequence([ge.regex(eat("hello")), ge.regex(eat("=")), ge.regex(eat("world")), ge.regex(eat("$"))])])))
+    SOFT_KEYWORDS = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    exprs.append(("file", ge.choice([ge.regex(eat(soft_keyword)) for soft_keyword in SOFT_KEYWORDS])))
 
 #     # TODO: delete this
 #     exprs.append(("start'''", ge.choice([ge.ref("file"), ge.regex(eat("hello"))])))
