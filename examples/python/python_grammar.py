a5f249f3616e8b581e05eb70b866bd77aad8e77f
IsaacBreen <mail@isaacbreen.com>
2025-05-04 11:19:34 +0800

    fk

--- examples/python/python_grammar.py
+++ examples/python/python_grammar.py
@@ -160,7 +160,8 @@ def pegen_to_sep1_grammar(grammar: pegen.grammar.Grammar) -> PyGrammar:
 #     exprs.append(("start'''", ge.ref("file")))
     # TODO: delete this
     # Add a rule for "hello=world$" to the start rule
-    exprs.append(("start'''", ge.choice([ge.ref("file"), ge.sequence([ge.regex(eat("hello")), ge.regex(eat("=")), ge.regex(eat("world")), ge.regex(eat("$"))])])))
+#     exprs.append(("start'''", ge.choice([ge.ref("file"), ge.sequence([ge.regex(eat("hello")), ge.regex(eat("=")), ge.regex(eat("world")), ge.regex(eat("$"))])])))
+    exprs.append(("start'''", ge.sequence([ge.regex(eat("hello")), ge.regex(eat("=")), ge.regex(eat("world")), ge.regex(eat("$"))])))
 
     for rule in grammar.rules.values():
         memo[rule.name] = ge.ref(rule.name)
