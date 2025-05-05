f9c276b306e6d228bb8d00c764248c5fc2a326f9
IsaacBreen <mail@isaacbreen.com>
2025-05-05 12:35:36 +0800

    hmmmmmm

--- examples/python/python_grammar.py
+++ examples/python/python_grammar.py
@@ -361,7 +361,7 @@ if __name__ == "__main__":
 #     input_text = "hello="
 #     expected_next_token = "world"
 
-    pre_input_text = "Hello"
+    pre_input_text = "x"
     input_text = ""
     expected_next_token = ""
 
