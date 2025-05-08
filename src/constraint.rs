59ab5bed8cca33a3d506c1598ef9e5ac84e8daa8
IsaacBreen <mail@isaacbreen.com>
2025-05-08 12:53:23 +0800

    fk

--- src/constraint.rs
+++ src/constraint.rs
@@ -210,7 +210,7 @@ impl GrammarConstraint {
         }
         // -----------------------------------------------------------
 
-        const MERGE_THRESHOLD: usize = 10;
+        const MERGE_THRESHOLD: usize = 2;
 
         // ----  Ord-capable handle for `Arc<Mutex<PrecomputeNode>>` --------------------------
         // `Arc<Mutex<PrecomputeNode>>` cannot live in a `BTreeSet` because `Mutex<T>` lacks
