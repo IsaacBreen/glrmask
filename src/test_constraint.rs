3c2cf482cfabda07ccef83fb449d937596a25092
IsaacBreen <mail@isaacbreen.com>
2025-05-13 16:30:15 +0800

    uh oh

--- src/test_constraint.rs
+++ src/test_constraint.rs
@@ -367,7 +367,8 @@ fn test_precompute_with_gpt2_vocab() -> Result<(), Box<dyn std::error::Error>> {
     // 2. Create a parser
     let productions = vec![
         prod("S", vec![nt("A"), t("DEF")]),
-        prod("S", vec![t("FSTRING_MIDDLE"), t("FSTRING_MIDDLE")]),
+        // prod("S", vec![t("FSTRING_MIDDLE"), t("FSTRING_MIDDLE")]),
+        prod("S", vec![t("FSTRING_MIDDLE")]),
     ];
     let terminal_map: BiBTreeMap<Terminal, TerminalID> = token_name_map.iter().map(|(name, id)| (Terminal(name.clone()), TerminalID(*id))).collect();
     let parser = generate_glr_parser_with_terminal_map(&productions, 0, terminal_map);
