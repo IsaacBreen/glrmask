c823b0e1a5b58906a5f47bbb4c93864ba1ae7dc9
IsaacBreen <mail@isaacbreen.com>
2025-05-13 12:14:19 +0800

    super simple

--- examples/python/python_grammar.py
+++ examples/python/python_grammar.py
@@ -185,7 +185,8 @@ def pegen_to_sep1_grammar(grammar: pegen.grammar.Grammar) -> PyGrammar:
 #     exprs.append(("start'''", ge.ref("IGNORE")))
 #     exprs.append(("start'''", ge.ref("FSTRING_MIDDLE")))
 
-    exprs.append(("start'''", ge.sequence([regex(eat("def")), ge.ref("NAME"), ge.regex(eat("(")), ge.ref("NAME"), ge.regex(eat(")"))])))
+#     exprs.append(("start'''", ge.sequence([regex(eat("def")), ge.ref("NAME"), ge.regex(eat("(")), ge.ref("NAME"), ge.regex(eat(")"))])))
+    exprs.append(("start'''", regex(eat("def"))))
 
 #     # TODO: delete this
 #     # Add a rule for "hello=world$" to the start rule
