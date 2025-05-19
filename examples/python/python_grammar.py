71e2044fdf20916f8fe726a2bd88f812de48108e
IsaacBreen <mail@isaacbreen.com>
2025-05-19 16:41:43 +0800

    ok

--- examples/python/python_grammar.py
+++ examples/python/python_grammar.py
@@ -367,21 +367,27 @@ if __name__ == "__main__":
 
     # Exclude tokens of length more than 3
 #     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if len(k) <= 2}
-    tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if len(k) <= 2}
+#     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if len(k) <= 2}
 #     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if len(k) <= 80}
 #     # Exclude tokens where any character appears more than once
 #     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if len(set(k)) == len(k)}
     # Exclude tokens that have any letter other than 'a'
 #     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if not any(c.isalpha() and c != 'a' for c in k)}
     # Exclude tokens that have any digit other than '1'
-    tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if not any(c.isdigit() and c != '1' for c in k)}
+#     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if not any(c.isdigit() and c != '1' for c in k)}
     # Exclude any non-alphanumeric non-whitespace character
     # Allow only ...
 #     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if all(c.isalnum() or c==' ' for c in k)}
 #     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if all(c.isalpha() or c==' ' for c in k)}
 #     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if all(c in 'a ' for c in k)}
 #     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if all(c=='a' for c in k)}
+#     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if all(c==' ' for c in k)}
+
+    # Really just using these three rn (19 May 2025)
+    tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if len(k) <= 2}
+    tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if not any(c.isdigit() and c != '1' for c in k)}
     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if all(c==' ' for c in k)}
+
 
     # Exclude tokens that have any character other than ...
 #     tokenizer_vocab = {k: v for k, v in tokenizer_vocab.items() if not any(c not in 'a1# ' for c in k)}
