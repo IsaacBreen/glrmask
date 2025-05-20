4545999e18be11214d978589ae4c895baf1c1367
IsaacBreen <mail@isaacbreen.com>
2025-05-20 15:45:01 +0800

    okkkk okok fk

--- src/interface/interface.rs
+++ src/interface/interface.rs
@@ -556,8 +556,10 @@ impl Display for CompiledGrammar {
         //     writeln!(f, "      {:?}: {:?}", expr, group_id)?;
         // }
 
-        writeln!(f, "  Tokenizer (States: {}): {}", self.tokenizer.dfa.states.len(), &self.tokenizer.dfa)?;
-        writeln!(f, "  GLR Parser (States: {}): {}", self.glr_parser.stage_7_table.len(), &self.glr_parser)?;
+        // writeln!(f, "  Tokenizer (States: {}): {}", self.tokenizer.dfa.states.len(), &self.tokenizer.dfa)?;
+        // writeln!(f, "  GLR Parser (States: {}): {}", self.glr_parser.stage_7_table.len(), &self.glr_parser)?;
+        writeln!(f, "  Tokenizer (States: {})", self.tokenizer.dfa.states.len())?;
+        writeln!(f, "  GLR Parser (States: {})", self.glr_parser.stage_7_table.len())?;
         Ok(())
     }
 }
