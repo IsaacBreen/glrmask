450042c36f09978e83f25ceafb03ffa4e9c51709
IsaacBreen <mail@isaacbreen.com>
2025-05-21 15:18:42 +0800

    fk

--- examples/python/simple_generate.py
+++ examples/python/simple_generate.py
@@ -53,7 +53,8 @@ def define_fruit_grammar_rules() -> List[Tuple[str, Any]]:
     rules: List[Tuple[str, Any]] = [("start_rule", ge.ref("sentences"))]
 
 #     rules.append(("sentences", ge.sequence([ge.ref("sentence"), ge.repeat(ge.regex(Regex.eat_u8(ord('\n')))), ge.ref("sentence")])))
-    rules.append(make_lexical_rule("sentences", ["the", "apple", "is", "a", "person"]))
+#     rules.append(make_lexical_rule("sentences", ["the", "apple", "is", "a", "person"]))
+    rules.append(("sentences", ge.literal("the apple is a person")))
 
     # IGNORE rule: optional spaces. This rule itself is not wrapped by IGNORE.
     # It allows zero or more spaces.
