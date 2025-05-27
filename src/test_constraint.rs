f3926a824bb8d4c88ed6aecd284f68d1d3034585
IsaacBreen <mail@isaacbreen.com>
2025-05-27 18:33:02 +0800

    hmmmmmmm

--- src/test_constraint.rs
+++ src/test_constraint.rs
@@ -849,9 +849,9 @@ fn test_constraint_from_serialized_compiled_grammar_and_gpt2_vocab() -> Result<(
     let tokenizer_vocab_tree = VocabPrefixTree::build(&vocab_tokens_for_tree);
 
     // The full text to tokenize.
-    // let full_text_to_tokenize = "from typing import Any";
+    let full_text_to_tokenize = "from typing import Any";
     // let full_text_to_tokenize = "((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((";
-    let full_text_to_tokenize = "a";
+    // let full_text_to_tokenize = "a";
 
     // Tokenize the full_text_to_tokenize using the VocabPrefixTree
     let mut test_token_sequence_ids = Vec::new();
