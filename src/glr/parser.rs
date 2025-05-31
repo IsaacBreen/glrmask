4969afc590691078f21339a9bf26febb1c8e4b99
IsaacBreen <mail@isaacbreen.com>
2025-05-31 16:03:08 +0800

    fixes

--- src/glr/parser.rs
+++ src/glr/parser.rs
@@ -448,11 +448,9 @@ impl<'a> GLRParserState<'a> { // No longer generic
     fn push_state(
         &self,
         stack: &Arc<GSSNode>, 
-        next_state_id: StateID,
+        new_content: ParseStateEdgeContent,
         acc_for_new_node: LLMTokenInfo,
-        user_data_for_new_edge: Arc<dyn UserDataTrait>, // New parameter
     ) -> ParseState {
-        let new_content = ParseStateEdgeContent { state_id: next_state_id, user_data: user_data_for_new_edge };
         let new_gss_node_instance = stack.push(new_content, acc_for_new_node);
         ParseState { stack: Arc::new(new_gss_node_instance) }
     }
@@ -482,7 +480,7 @@ impl<'a> GLRParserState<'a> { // No longer generic
                     crate::debug!(4, " ...and edge value {:?}, predecessor {:p}, goto state ID {}", edge_value.state_id, Arc::as_ptr(&predecessor_arc), goto_state_id.0);
 
                     let new_acc_for_goto_child = parent_gss_node.acc().clone().intersect(cur_acc_from_reducible_node.clone());
-                    let goto_node_content = ParseStateEdgeContent { state_id: goto_state_id };
+                    let goto_node_content = ParseStateEdgeContent { state_id: goto_state_id, user_data: edge_content.user_data.clone() };
 
                     let isolated_parent_arc = Arc::new(predecessor_arc.push(edge_value, new_acc_for_goto_child.clone()));
                     let new_gss_node = isolated_parent_arc.push(goto_node_content, new_acc_for_goto_child);
@@ -582,7 +580,8 @@ impl<'a> GLRParserState<'a> { // No longer generic
                 match row.shifts_and_reduces.get(&token_id) {
                     Some(Stage7ShiftsAndReduces::Shift(to)) => {
                         crate::debug!(4, "Shift from state {} via token {} to state {}", top_edge_content.state_id.0, token_id.0, to.0);
-                        let new_parse_state = self.push_state(&temp_idk, *to, stack_arc_for_operations.acc().clone());
+                        let new_content = ParseStateEdgeContent { state_id: *to, user_data: top_edge_content.user_data.clone() };
+                        let new_parse_state = self.push_state(&temp_idk, new_content, stack_arc_for_operations.acc().clone());
                         next.merge(new_parse_state);
                     }
 
@@ -601,7 +600,8 @@ impl<'a> GLRParserState<'a> { // No longer generic
                         crate::debug!(4, "Split from state {} via token {}", top_edge_content.state_id.0, token_id.0);
                         if let Some(to) = shift {
                             crate::debug!(4, " Shift from state {} via token {} to state {}", top_edge_content.state_id.0, token_id.0, to.0);
-                            let new_parse_state = self.push_state(&temp_idk, *to, stack_arc_for_operations.acc().clone());
+                            let new_content = ParseStateEdgeContent { state_id: *to, user_data: top_edge_content.user_data.clone() };
+                            let new_parse_state = self.push_state(&temp_idk, new_content, stack_arc_for_operations.acc().clone());
                             next.merge(new_parse_state);
                         }
                         for (len, nts) in reduces {
