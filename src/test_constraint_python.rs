44d6bbd3644f9cd185a53e1a18e1ca93cc189478
IsaacBreen <mail@isaacbreen.com>
2025-06-06 14:35:37 +0800

    okook

--- src/test_constraint_python.rs
+++ src/test_constraint_python.rs
@@ -631,6 +631,9 @@ fn test_constraint_from_serialized_compiled_grammar_and_gpt2_vocab() -> Result<(
     // let full_text_to_tokenize = "import os\nimport sys";
     //     let full_text_to_tokenize = "# Top-level comment, challenging parser start\nimport os";
     // let full_text_to_tokenize = "import os";
+    // f-strings
+    let full_text_to_tokenize = "x = f'hi!'\n"; 
+    let full_text_to_tokenize = "x = f'hi{x}'\n";
 
     // Tokenize the full_text_to_tokenize using the VocabPrefixTree
     let mut test_token_sequence_ids = Vec::new();
