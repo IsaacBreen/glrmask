fe3483f8dbc596872e4a9473ecd2df20df216d1d
IsaacBreen <mail@isaacbreen.com>
2025-06-07 15:26:54 +0800

    okoko

--- src/constraint.rs
+++ src/constraint.rs
@@ -1150,6 +1150,7 @@ impl<'a> GrammarConstraintState<'a> {
                     for (terminal_id, llm_tokens_that_match_this_terminal) in possible_matches_for_state {
                         if !allowed_terminals_for_state.contains(terminal_id.0) {
                             // This terminal is not allowed
+                            crate::debug!(4, "Subtracting forbidden LLM tokens for terminal {:?} (ID {}) for state {}: {:?}", self.parent.parser.terminal_map.get_by_right(terminal_id).map(|t| t.0.clone()).unwrap_or("UNKNOWN_TERMINAL".to_string()), terminal_id.0, tokenizer_state_id.0, llm_tokens_that_match_this_terminal);
                             forbidden_llm_tokens |= llm_tokens_that_match_this_terminal;
                         }
                     }
