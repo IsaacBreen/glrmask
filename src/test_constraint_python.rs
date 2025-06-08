3a148355ab7e173774f55f3271f0f2374e2e1019
IsaacBreen <mail@isaacbreen.com>
2025-06-08 15:31:39 +0800

    okkkkkkkkk

--- src/test_constraint_python.rs
+++ src/test_constraint_python.rs
@@ -708,7 +708,8 @@ fn test_constraint_from_serialized_compiled_grammar_and_gpt2_vocab() -> Result<(
     // let full_text_to_tokenize = "x = f'hi{x}'\n";
     // let full_text_to_tokenize = "f'hi{x}'\n";
     // let full_text_to_tokenize = "                        ";
-    let full_text_to_tokenize = "# Top-level comment, challenging parser start\nimport os, sys # Multiple imports on one line\nfrom collections import (defaultdict,\n                         deque) # Multi-line import with parens\n\nGLOBAL_VAR: int = 100";
+    // let full_text_to_tokenize = "# Top-level comment, challenging parser start\nimport os, sys # Multiple imports on one line\nfrom collections import (defaultdict,\n                         deque) # Multi-line import with parens\n\nGLOBAL_VAR: int = 100";
+    let full_text_to_tokenize = "import x\nAAAAAAAAAAAAAAAAAAAAAA";
 
     // Tokenize the full_text_to_tokenize using the VocabPrefixTree
     let mut test_token_sequence_ids = Vec::new();
