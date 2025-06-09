9f52daba5b245546e4832b9c5dcce49d0cf17f19
IsaacBreen <mail@isaacbreen.com>
2025-06-09 12:04:12 +0800

    Feat: Enhance profiler macros with optional `time_it` name and new `timeit!` expression macro.

--- profiler_macro/src/lib.rs
+++ profiler_macro/src/lib.rs
@@ -1,26 +1,65 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn, LitStr};
+use syn::{
+    parse::{Parse, ParseStream},
+    parse_macro_input, Expr, ItemFn, LitStr, Token,
+};
 
 #[proc_macro_attribute]
 pub fn time_it(attr: TokenStream, item: TokenStream) -> TokenStream {
     let item_fn = parse_macro_input!(item as ItemFn);
-    let attr_str = parse_macro_input!(attr as LitStr);
+
+    let timer_name_expr = if attr.is_empty() {
+        let fn_name = item_fn.sig.ident.to_string();
+        quote! { format!("{}::{}", module_path!(), #fn_name) }
+    } else {
+        let attr_str = parse_macro_input!(attr as LitStr);
+        let name = attr_str.value();
+        quote! { String::from(#name) }
+    };
 
     let fn_vis = &item_fn.vis;
     let fn_sig = &item_fn.sig;
     let fn_block = &item_fn.block;
     let fn_attrs = &item_fn.attrs;
 
-    let timer_name = attr_str.value();
-
     let result = quote! {
         #(#fn_attrs)*
         #fn_vis #fn_sig {
-            let _guard = crate::profiler::TimedBlockGuard::new(String::from(#timer_name));
+            let _guard = crate::profiler::TimedBlockGuard::new(#timer_name_expr);
             #fn_block
         }
     };
+
+    result.into()
+}
+
+struct TimeitArgs {
+    name: LitStr,
+    _comma: Token![,],
+    expr: Expr,
+}
+
+impl Parse for TimeitArgs {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        Ok(TimeitArgs {
+            name: input.parse()?,
+            _comma: input.parse()?,
+            expr: input.parse()?,
+        })
+    }
+}
+
+#[proc_macro]
+pub fn timeit(input: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(input as TimeitArgs);
+    let name = args.name.value();
+    let expr = &args.expr;
+
+    let result = quote! {{
+        let _guard = crate::profiler::TimedBlockGuard::new(String::from(#name));
+        #expr
+    }};
 
     result.into()
 }
