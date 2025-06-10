e51a80dc8a87d636a65ce3a3576db0fb14145aa0
IsaacBreen <mail@isaacbreen.com>
2025-06-10 12:18:54 +0800

    hmm

--- src/test_constraint_python.rs
+++ src/test_constraint_python.rs
@@ -624,7 +624,9 @@ fn test_constraint_from_serialized_compiled_grammar_and_gpt2_vocab() -> Result<(
 
     // Keep tokens that are either given length or of the form ' Ax' where x is a letter
     llm_token_map.retain(|v, _| {
-        v.len() <= 1 || v.starts_with(b" A") && v.len() <= 3
+        // v.len() <= 1 || v.starts_with(b" A") && v.len() <= 3
+        v.len() <= 1 || (v.starts_with(b" A")) && v.len() <= 3
+        // v.len() <= 3
     });
 
     // Print the vocab
