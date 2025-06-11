4fbdf1d7a8fda01e3242ff95afb8f06e94f96518
IsaacBreen <mail@isaacbreen.com>
2025-06-11 11:45:11 +0800

    FFFFFFFFFFf

--- src/test_constraint_python.rs
+++ src/test_constraint_python.rs
@@ -648,6 +648,7 @@ fn test_constraint_from_serialized_compiled_grammar_and_gpt2_vocab() -> Result<(
 
     // Keep only "1" and "11"
     // llm_token_map.retain(|v, _| v.len() == 1 || v == b"1" || v == b"11");
+    llm_token_map.retain(|v, _| v == b"1" || v == b"11");
 
     // Print the vocab
     println!("GPT-2 vocab loaded and processed into LLMTokenMap ({} tokens, max_original_id: {}).", llm_token_map.len(), max_original_llm_token_id_val);
