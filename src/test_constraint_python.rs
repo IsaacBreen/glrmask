100d17d2e70f04ea0abbb1f974f29e0ffd034d18
IsaacBreen <mail@isaacbreen.com>
2025-06-13 19:28:43 +0800

    fffffffrick

--- src/test_constraint_python.rs
+++ src/test_constraint_python.rs
@@ -558,13 +558,13 @@ fn test_constraint_from_serialized_compiled_grammar_and_gpt2_vocab() -> Result<(
     // let vocab_file_name = "qwen_vocab.json";
     let mut gpt2_raw_vocab = load_or_download_gpt2_vocab(cache_dir, vocab_file_name, vocab_url)?;
     // let gpt2_raw_vocab = BTreeMap::from([("________________________________________________________________", 0)]);
-    // let mut gpt2_raw_vocab = BTreeMap::new();
-    // // Just fill with all bytes
-    // for i in 0u8..=255u8 {
-    //     let c = i as char;
-    //     let s = c.to_string();
-    //     gpt2_raw_vocab.insert(s, i as usize);
-    // }
+    // Just fill with all bytes
+    let mut gpt2_raw_vocab = BTreeMap::new();
+    for i in 0u8..=255u8 {
+        let c = i as char;
+        let s = c.to_string();
+        gpt2_raw_vocab.insert(s, i as usize);
+    }
     // gpt2_raw_vocab.insert("import os".to_string(), 0);
     // gpt2_raw_vocab.insert("import ".to_string(), 1);
     // gpt2_raw_vocab.insert(" os".to_string(), 2);
@@ -963,7 +963,14 @@ fn test_minimize_grammar_for_mask_bug() -> Result<(), Box<dyn std::error::Error>
     let cache_dir = Path::new(".cache/test_vocabs");
     let vocab_url = "https://huggingface.co/openai-community/gpt2/raw/main/vocab.json";
     let vocab_file_name = "gpt2_vocab.json";
-    let gpt2_raw_vocab = load_or_download_gpt2_vocab(cache_dir, vocab_file_name, vocab_url)?;
+    // let gpt2_raw_vocab = load_or_download_gpt2_vocab(cache_dir, vocab_file_name, vocab_url)?;
+    // Just fill with all bytes
+    let mut gpt2_raw_vocab = BTreeMap::new();
+    for i in 0u8..=255u8 {
+        let c = i as char;
+        let s = c.to_string();
+        gpt2_raw_vocab.insert(s, i as usize);
+    }
     
     let mut llm_token_map = LLMTokenMap::new();
     for (token_str, id_val_u32) in gpt2_raw_vocab {
