fe2dfb326ddaeefce143a175632d22de17fb607d
IsaacBreen <mail@isaacbreen.com>
2025-06-13 13:09:20 +0800

    ok!

--- src/constraint.rs
+++ src/constraint.rs
@@ -759,12 +759,12 @@ impl<'r> Precomputer<'r> {
                             let next_tokenizer_state = self.tokenizer.initial_state_id();
                             let dest_nodes_in_queue = work_queue.entry(next_pos).or_default().entry(next_tokenizer_state).or_default();
 
-                            inserter = inserter.try_destinations_iter(dest_nodes_in_queue.iter().map(|w| w.as_arc().clone()));
+                            inserter = inserter.try_destinations_iter(dest_nodes_in_queue.iter().map(|w| w.as_arc().clone()).filter(|w| !w.lock().unwrap().value.end));
 
                             let children_of_src: Vec<_> = src_node_wrapper.lock().unwrap().children().values().flat_map(|m| m.keys().cloned()).collect();
                             let tags = self.tags.borrow();
                             let eligible_children = children_of_src.iter().filter(|child_wrapper| {
-                                tags.get(child_wrapper).map_or(true, |tag| (tag & &edge_bv).is_empty())
+                                tags.get(child_wrapper).map_or(true, |tag| (tag & &edge_bv).is_empty()) && !child_wrapper.lock().unwrap().value.end
                             }).map(|w| w.as_arc().clone());
                             inserter = inserter.try_destinations_iter(eligible_children);
                             drop(tags);
