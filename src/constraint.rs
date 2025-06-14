3b1f4fb70b418877a905652cab837c8bab289072
IsaacBreen <mail@isaacbreen.com>
2025-06-14 14:23:40 +0800

    f

--- src/constraint.rs
+++ src/constraint.rs
@@ -338,8 +338,8 @@ impl GrammarConstraint {
         );
 
         helper.run_dfs();
-        // helper.prune_precomputed_graph();
-        // helper.prune_terminal_sequences();
+        // helper.prune_on_no_terminal_follow();
+        // helper.prune_dead_paths();
         helper.merge_nodes();
         helper.finish(token_name_map, possible_matches, internal_max_llm_token)
     }
