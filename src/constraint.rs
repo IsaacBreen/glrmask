9d324416f59edffc5fd85f2b8a5a43d11f515a92
IsaacBreen <mail@isaacbreen.com>
2025-06-14 20:50:14 +0800

    hmmmmmm

--- src/constraint.rs
+++ src/constraint.rs
@@ -989,11 +989,12 @@ impl<'a> GrammarConstraintState<'a> {
             |glr_s, grammar_token_opt, dest_map| {
                 timeit!("get_mask step_fn", {
                     let mut results = Vec::new();
-                    let mut glr_s = glr_s.clone();
+                    // let mut glr_s = glr_s.clone();
                     // glr_s.log_gss("After stepping", grammar_token_opt.unwrap_or(TerminalID(0)));
 
                     crate::debug!(4, "After stepping with grammar_token_opt: {:?}", glr_s.is_ok());
                     for (child_node_trie_data, edge_llm_tokens_bv) in dest_map.iter() {
+                        let mut glr_s = glr_s.clone();
                         subtract_llm_tokens_and_prune_arc(&mut glr_s.active_state.stack, &final_mask_internal.borrow(), &mut HashMap::new());
                         intersect_llm_tokens_and_prune_arc(&mut glr_s.active_state.stack, &edge_llm_tokens_bv, &mut HashMap::new());
                         crate::debug!(4, "Stepping with grammar_token_opt: {:?}", grammar_token_opt);
@@ -1002,7 +1003,6 @@ impl<'a> GrammarConstraintState<'a> {
                             *step_counts_clone1.lock().unwrap().entry(*gtid).or_insert(0) += 1;
                             glr_s.step(*gtid);
                         }
-                        let mut glr_s = glr_s.clone();
                         crate::debug!(4, "Intersecting with edge_llm_tokens_bv: {:?}", edge_llm_tokens_bv);
                         // subtract_llm_tokens_and_prune_arc(&mut glr_s.active_state.stack, &final_mask_internal.borrow(), &mut HashMap::new());
                         // glr_s.log_gss("After intersecting", grammar_token_opt.unwrap_or(TerminalID(0)));
