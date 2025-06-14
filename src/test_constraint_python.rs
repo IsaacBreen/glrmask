c42de63b51075857cc1b345961c5de95f1fa2c02
IsaacBreen <mail@isaacbreen.com>
2025-06-14 12:42:01 +0800

    f

--- src/test_constraint_python.rs
+++ src/test_constraint_python.rs
@@ -649,11 +649,10 @@ fn test_constraint_from_serialized_compiled_grammar_and_gpt2_vocab() -> Result<(
     // // llm_token_map.retain(|v, _| v.len() == 1 || v == b"1" || v == b"11");
     // // llm_token_map.retain(|v, _| v == b"1" || v == b"11");
 
-    // gpt2_raw_vocab.retain(|k, _| [b"from".as_ref(), b" x"].contains(&k.as_ref()) || k.len() <= 2);
     // llm_token_map.retain(|v, _| [b"from".as_ref(), b" x".as_ref()].contains(&v.as_ref()) || v.len() == 2 && v.iter().all(|c| !c.is_ascii_alphanumeric()));
     // llm_token_map.retain(|v, _| [b"from".as_ref(), b" x".as_ref(), ].contains(&v.as_ref()) || v.len() == 2 && v.iter().all(|c| !c.is_ascii_alphanumeric()) && *String::from_utf8_lossy(v.as_ref()) <= *"\"[");
     // llm_token_map.retain(|v, _| [b"from".as_ref(), b" x".as_ref(), ].contains(&v.as_ref()) || v.len() == 2 && v.iter().all(|c| !c.is_ascii_alphanumeric()) && *" `" <= *String::from_utf8_lossy(v.as_ref()) && *String::from_utf8_lossy(v.as_ref()) <= *"\"[");
-    gpt2_raw_vocab.retain(|k, _| [b"from".as_ref(), b" x", b" _"].contains(&k.as_ref()));
+    llm_token_map.retain(|v, _| [b"from".as_ref(), b" x", b" _"].contains(&v.as_ref()));
     assert!(llm_token_map.contains_left(&b"from".to_vec()));
     assert!(llm_token_map.contains_left(&b" x".to_vec()));
 
