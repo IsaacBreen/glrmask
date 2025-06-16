432c1f61c6659b0f53670bc8a5c254234ebc8cf9
IsaacBreen <mail@isaacbreen.com>
2025-06-16 20:53:37 +0800

    f

--- src/glr/parser.rs
+++ src/glr/parser.rs
@@ -594,7 +594,7 @@ impl<'a> GLRParserState<'a> { // No longer generic
         let make_msg = |print_full_forest, max_nodes_to_print| {
             if print_full_forest {
                 format!("GSS ({} nodes):\n{}", stats.unique_nodes,
-                        print_gss_forest(&roots, max_nodes_to_print))
+                        print_gss_forest(&roots, max_nodes_to_print, &self.parser.terminal_map))
             } else {
                 match find_longest_path(&self.active_state.stack) {
                     Some(p) => format!("GSS too big ({} nodes). Longest path ({}): {}",
