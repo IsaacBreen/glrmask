655a52c89f224d67a24122fcce22fac91301ea25
IsaacBreen <mail@isaacbreen.com>
2025-06-17 18:25:56 +0800

    fk

--- src/example_code.py
+++ src/example_code.py
@@ -1,6 +1,3 @@
-async def main():
-    sample_data = [
-        {'values': [1, 2, 0.6, "hello", None, 5.0]},
-        {'values': [10, "world", 0.1, 30]},
-        {}
-    ]
+ANOTHER_GLOBAL = r"C:\raw\string\path"
+
+{}
