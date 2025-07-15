8b7fc6cb2519c5f8fcd7a49f52cfc0773c7cf475
IsaacBreen <mail@isaacbreen.com>
2025-07-15 15:06:57 -0700

    okkk

--- src/glr/parser.rs
+++ src/glr/parser.rs
@@ -581,7 +581,7 @@ impl<'a> GLRParserState<'a> { // No longer generic
 
     // #[time_it("GLRParserState::step")]
     pub fn step(&mut self, token_id: TerminalID) {
-        timeit!(format!("GLRParserState::step({})", self.parser.terminal_map.get_by_right(&token_id).unwrap()), {
+        timeit!(format!("GLRParserState::step({})", self.parser.terminal_map.get_by_right(&token_id).map_or("Unknown".to_string(), |t| t.to_string())), {
         if Some(token_id) == self.parser.ignore_terminal_id {
             crate::debug!(4, "Ignoring token {} ({:?})", token_id.0, self.parser.terminal_map.get_by_right(&token_id));
             return;
