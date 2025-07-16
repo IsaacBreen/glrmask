f64039be3813c2fc434396a3dd1b64c5ab2e465c
IsaacBreen <mail@isaacbreen.com>
2025-07-16 13:57:06 -0700

    fix

--- src/interface/interface.rs
+++ src/interface/interface.rs
@@ -995,7 +995,7 @@ impl Display for CompiledGrammar {
         // }
 
         writeln!(f, "  Tokenizer (States: {}): {}", self.tokenizer.dfa.states.len(), &self.tokenizer.dfa)?;
-        writeln!(f, "  GLR Parser (States: {}): {}", self.glr_parser.stage_7_table.len(), &self.glr_parser)?;
+        writeln!(f, "  GLR Parser (States: {}): {}", self.glr_parser.stage_8_table.len(), &self.glr_parser)?;
         Ok(())
     }
 }
