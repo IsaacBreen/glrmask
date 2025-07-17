19e58780f8f41595e30845c090f601c40b5f58de
IsaacBreen <mail@isaacbreen.com>
2025-07-16 22:02:07 -0700

    hm

--- src/glr/parser.rs
+++ src/glr/parser.rs
@@ -638,13 +638,13 @@ impl<'a> GLRParserState<'a> { // No longer generic
                          }) => {
                         let nonterminal = self.parser.non_terminal_map.get_by_right(nt).unwrap();
                         // timeit!("GLRParserState::step::reduce", {
-                        // timeit!(format!("GLRParserState::step::reduce ({} -> {}) (pids {:?})", nonterminal.0, len, production_ids), {
+                        timeit!(format!("GLRParserState::step::reduce ({} -> {}) (pids {:?})", nonterminal.0, len, production_ids), {
                         crate::debug!(4, "Reduce from state {} via token {} to nonterminal {} of length {}", peek.edge_value().state_id.0, token_id.0, nonterminal.0, len);
                         let s_new_arc = self.reduce_and_goto(&peek, *nt, *len);
                         if !s_new_arc.is_empty() { // Only add to todo if the reduction leads to valid states
                            todo.push(ParseState { stack: s_new_arc });
                         }
-                        // })
+                        })
                     }
 
                     Some(Stage7ShiftsAndReduces::Split { shift, reduces }) => {
