4adb0c1b5356ac902de5a42174ed39935d3e6d40
IsaacBreen <mail@isaacbreen.com>
2025-07-23 16:04:01 -0700

    fixes maybe

--- src/glr/items.rs
+++ src/glr/items.rs
@@ -64,21 +64,79 @@ impl Display for Item {
     }
 }
 
+impl Item {
+    pub fn dot_at_end(&self) -> bool {
+        self.dot_position == self.production.rhs.len()
+    }
+
+    pub fn next(&self) -> Self {
+        // Create a new item with the dot moved one position to the right
+        if self.dot_position < self.production.rhs.len() {
+            Item {
+                production: self.production.clone(),
+                dot_position: self.dot_position + 1,
+                lookahead: self.lookahead.clone(),
+            }
+        } else {
+            // If the dot is at the end, return self
+            self.clone()
+        }
+    }
+}
+
+pub fn compute_lookaheads(
+    item: &Item,
+    productions: &[Production],
+    first_sets: &BTreeMap<NonTerminal, BTreeSet<Terminal>>,
+    nullable_nonterminals: &BTreeSet<NonTerminal>,
+) -> BTreeSet<Terminal> {
+    match item.production.rhs.get(item.dot_position + 1) {
+        Some(Symbol::Terminal(t)) => {
+            // If the next symbol is a terminal, the lookahead is just that terminal
+            BTreeSet::from([t.clone()])
+        }
+        Some(Symbol::NonTerminal(nt)) => {
+            let first_set = first_sets.get(nt).cloned().unwrap_or_default();
+            if nullable_nonterminals.contains(nt) {
+                // If the non-terminal is nullable, we also need to include the lookaheads for the next item
+                let next_lookaheads = compute_lookaheads(
+                    &item.next(),
+                    productions,
+                    first_sets,
+                    nullable_nonterminals,
+                );
+                first_set.union(&next_lookaheads).cloned().collect()
+            } else {
+                first_set
+            }
+        }
+        None => {
+            // The child production is of length 0. Inherit the lookahead from the parent production.
+            if let Some(lookahead) = &item.lookahead {
+                BTreeSet::from([lookahead.clone()])
+            } else {
+                BTreeSet::new() // No lookahead if none is provided
+            }
+        }
+    }
+}
+
 pub fn compute_closure(items: &BTreeSet<Item>, productions: &[Production]) -> BTreeSet<Item> {
     // crate::debug!(3, "Computing closure");
-    let follow_sets = compute_follow_sets(productions);
+    let first_sets = compute_first_sets(productions);
+    let nullable_nonterminals = compute_epsilon_nonterminals(productions);
     let mut closure = items.clone();
     let mut worklist: VecDeque<Item> = items.iter().cloned().collect();
 
     while let Some(item) = worklist.pop_front() {
         if let Some(Symbol::NonTerminal(nt)) = item.production.rhs.get(item.dot_position) {
             for prod in productions.iter().filter(|p| p.lhs == *nt) {
-                let follows = follow_sets.get(&nt).cloned().unwrap_or_default();
-                for lookahead in follows {
+                let lookaheads = compute_lookaheads(&item.next(), productions, &first_sets, &nullable_nonterminals);
+                for lookahead in lookaheads {
                     let new_item = Item {
                         production: prod.clone(),
                         dot_position: 0,
-                        lookahead: Some(lookahead.clone()),
+                        lookahead: Some(lookahead),
                     };
                     if closure.insert(new_item.clone()) {
                         worklist.push_back(new_item);
