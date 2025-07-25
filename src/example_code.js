b082f233b19f5b5cce6f369e9090890a8afb20e1
IsaacBreen <mail@isaacbreen.com>
2025-07-25 14:44:51 -0700

    ohh

--- src/example_code.js
+++ src/example_code.js
@@ -1,10 +1,11 @@
+
+let aaaaaaaa = 11111111111111111111111111111111;
 const myObject = {
     // Numeric keys
     123: "numeric key", // Numeric keys
     // Computed property name
+    [Symbol('description')]: "symbol key value",
 };
-
-let aaaaaaaa = 11111111111111111111111111111111;
 let aaaaaaaa = 11111111111111111111111111111111;
 let aaaaaaaa = 11111111111111111111111111111111;
 aaaaa;
