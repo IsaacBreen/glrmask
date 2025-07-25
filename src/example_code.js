00dd36c139a07b36109caa29efe22a3299845560
IsaacBreen <mail@isaacbreen.com>
2025-07-25 16:21:03 -0700

    wtf

--- src/example_code.js
+++ src/example_code.js
@@ -1,9 +1,12 @@
+[x]
+[Symbol('description')]
 
 let aaaaaaaa = 11111111111111111111111111111111;
 const myObject = {
     // Numeric keys
     123: "numeric key", // Numeric keys
     // Computed property name
+    [x]: "symbol key value",
     [Symbol('description')]: "symbol key value",
 };
 let aaaaaaaa = 11111111111111111111111111111111;
