20dadc75945cc933b4e2022009ef0c9780ca5c9f
IsaacBreen <mail@isaacbreen.com>
2025-07-24 23:22:26 -0700

    pop last

--- src/glr/parser.rs
+++ src/glr/parser.rs
@@ -848,7 +848,7 @@ impl<'a> GLRParserState<'a> { // No longer generic
 
         crate::debug!(4, "Phase 3: Processing {} states", work_map.len());
         timeit!(format!("GLRParserState::step::phase3 - unique_nodes: {}", stats.unique_nodes), {
-            while let Some(((_depth, state_id), state)) = work_map.pop_first() {
+            while let Some(((_depth, state_id), state)) = work_map.pop_last() {
                 let row = &self.parser.stage_7_table[&state_id];
 
                 if let Some(ref r) = row.phase3_default_reduce.reduce {
