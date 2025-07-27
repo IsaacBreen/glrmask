43b9bbd57313a2ff31d96abe83a1a64c75e5aa16
IsaacBreen <mail@isaacbreen.com>
2025-07-26 19:59:34 -0700

    hm

--- src/test_constraint_js.rs
+++ src/test_constraint_js.rs
@@ -204,11 +204,7 @@ fn test_js_constraint_integration() -> Result<(), Box<dyn std::error::Error>> {
 
         // Option 3: Set to a few specific tokens
         gpt2_raw_vocab = vec![
-            // "x",
-            // "[",
-            "[x",
-            // "]",
-            "]:",
+            "[x]:",
             " &",
         ].iter().map(|s| s.to_string()).collect();
 
