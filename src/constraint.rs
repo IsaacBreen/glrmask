c9a2ed9e25a9fd457c00a9af4998ed73d9139a85
IsaacBreen <mail@isaacbreen.com>
2025-07-28 17:21:51 -0700

    merge...

--- src/constraint.rs
+++ src/constraint.rs
@@ -874,7 +874,7 @@ impl<'r> Precomputer<'r> {
 
                             inserter = inserter.try_destinations_iter(dest_nodes_in_queue.iter().map(|w| w.as_arc().clone()).filter(|w| !w.lock().unwrap().value.end));
 
-                            if false {
+                            if true {
                                 let children_of_src: Vec<_> = src_node_wrapper.lock().unwrap().children().values().flat_map(|m| m.keys().cloned()).collect();
                                 let tags = self.tags.borrow();
                                 let eligible_children = children_of_src.iter().filter(|child_wrapper| {
