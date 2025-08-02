2377152b751a00cc621d4ec544c0ffecac430454
IsaacBreen <mail@isaacbreen.com>
2025-08-01 18:42:06 -0700

    fk

--- src/glr/parser.rs
+++ src/glr/parser.rs
@@ -977,7 +977,7 @@ impl<'a> GLRParserState<'a> { // No longer generic
                                 let mut current_nt = nt;
                                 println!("loop start");
                                 loop {
-                                    println!("loop current_nt: {}", current_nt);
+                                    println!("loop current_nt: {:?}", current_nt);
                                     let goto = self.parser.table.get(&state_id).and_then(|row| row.gotos.get(&current_nt)).expect(
                                         format!("Goto not found for NT '{}' in state {:?}", self.parser.non_terminal_map.get_by_right(&current_nt).unwrap(), state_id).as_str()
                                     );
