f9091189bd9bb6239f24b53df9fe36832e6597e7
IsaacBreen <mail@isaacbreen.com>
2025-08-02 12:33:58 -0700

    ok

--- src/glr/parser.rs
+++ src/glr/parser.rs
@@ -1030,7 +1030,22 @@ impl<'a> GLRParserState<'a> { // No longer generic
                                     }
                                 }
                                 if i > 1 {
-                                    println!("number of loops: {:5}, number of goto_state_ids: {:?}", i, goto_state_ids.len());                                    // panic!("number of loops: {}", i);
+                                    println!("number of loops: {:5}, number of goto_state_ids: {}", i, goto_state_ids.len());
+
+                                    // Round to nearest power of 2
+                                    let i_rounded_to_nearest_pow = if i == 0 {
+                                        1
+                                    } else {
+                                        1 << (32 - (i as u32 - 1).leading_zeros())
+                                    };
+
+                                    let goto_state_ids_rounded_to_nearest_pow = if goto_state_ids.len() == 0 {
+                                        1
+                                    } else {
+                                        1 << (32 - (goto_state_ids.len() as u32 - 1).leading_zeros())
+                                    };
+
+                                    timeit!(format!("GLRParserState::step::phase2::goto::number of loops (rounded to nearest pow of 4), number of goto_state_ids (rounded to nearest pow): {:5}, {:5}", i_rounded_to_nearest_pow, goto_state_ids_rounded_to_nearest_pow), {});
                                 }
 
                                 let new_gss_node = peek2.isolated_parent().push_many(goto_state_ids.into_iter().map(|state_id| ParseStateEdgeContent { state_id }).collect());
