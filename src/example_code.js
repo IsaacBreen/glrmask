21cfbe9a61524432b50332f40e9485e0460b673a
IsaacBreen <mail@isaacbreen.com>
2025-08-04 17:28:28 -0700

    f

--- src/example_code.js
+++ src/example_code.js
@@ -1,20 +1,4 @@
 class Dog extends Animal {
-    constructor(name, breed) {
-        super(name); // Call to super()
-        this.breed = breed;
-    }
-
-    // Overriding method
-    speak() {
-        super.speak(); // Call to super method
-        console.log(`${this.name} barks.`);
-    }
-
-    // Getter
-    get description() {
-        return `${this.name} is a ${this.breed}.`;
-    }
-
     // Setter
     set nickname(nick) {
         this._nickname = nick;
