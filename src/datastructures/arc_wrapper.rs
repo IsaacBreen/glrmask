5edcca84e6c6637ce466fb35386022fe626fc862
IsaacBreen <mail@isaacbreen.com>
2025-08-12 12:39:47 -0700

    feat: Add WeakPtrWrapper for pointer-based identity of Weak<T>

--- src/datastructures/arc_wrapper.rs
+++ src/datastructures/arc_wrapper.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::sync::{Arc, Weak};
 use std::ops::Deref;
 use std::fmt;
 use std::hash::{Hash, Hasher};
@@ -10,6 +10,7 @@ use crate::json_serialization::{JSONConvertible, JSONNode}; // Added
 /// This allows `Arc<T>` instances to be used in collections like `BTreeSet` or
 /// `HashMap` where identity is determined by the `Arc`'s pointer, not its content.
 /// It also dereferences to the underlying `Arc<T>`.
+#[derive(Clone)]
 pub struct ArcPtrWrapper<T>(Arc<T>);
 
 // ArcPtrWrapper serialization:
@@ -26,6 +27,67 @@ impl<T: JSONConvertible> JSONConvertible for ArcPtrWrapper<T> {
     }
 }
 
+/// A wrapper around `Weak<T>` that implements `PartialEq`, `Eq`, `PartialOrd`, `Ord`,
+/// and `Hash` based on the pointer value of the `Weak`.
+/// This allows `Weak<T>` instances to be used as map/set keys where identity is
+/// determined by the allocation pointer, not its content.
+///
+/// Note:
+/// - `Weak<T>` may fail to upgrade; code using this wrapper should be prepared
+///   to skip dangling weak references.
+#[derive(Clone)]
+pub struct WeakPtrWrapper<T>(Weak<T>);
+
+impl<T> WeakPtrWrapper<T> {
+    /// Creates a new `WeakPtrWrapper` from a `Weak<T>`.
+    pub fn new(weak: Weak<T>) -> Self {
+        WeakPtrWrapper(weak)
+    }
+
+    /// Returns a reference to the inner `Weak<T>`.
+    pub fn as_weak(&self) -> &Weak<T> {
+        &self.0
+    }
+
+    /// Attempts to upgrade the weak reference to an `Arc<T>`.
+    pub fn upgrade(&self) -> Option<Arc<T>> {
+        self.0.upgrade()
+    }
+
+    /// Consumes the wrapper and returns the inner `Weak<T>`.
+    pub fn into_weak(self) -> Weak<T> {
+        self.0
+    }
+}
+
+impl<T> Deref for WeakPtrWrapper<T> {
+    type Target = Weak<T>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> PartialEq for WeakPtrWrapper<T> {
+    fn eq(&self, other: &Self) -> bool {
+        Weak::as_ptr(&self.0) == Weak::as_ptr(&other.0)
+    }
+}
+
+impl<T> Eq for WeakPtrWrapper<T> {}
+
+impl<T> Hash for WeakPtrWrapper<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        (Weak::as_ptr(&self.0) as usize).hash(state);
+    }
+}
+
+impl<T> fmt::Debug for WeakPtrWrapper<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("WeakPtrWrapper")
+         .field(&(Weak::as_ptr(&self.0) as *const ()))
+         .finish()
+    }
+}
 
 impl<T> ArcPtrWrapper<T> {
     /// Creates a new `ArcPtrWrapper` from an `Arc<T>`.
@@ -41,12 +103,6 @@ impl<T> ArcPtrWrapper<T> {
     /// Consumes the wrapper and returns the inner `Arc<T>`.
     pub fn into_arc(self) -> Arc<T> {
         self.0
-    }
-}
-
-impl<T> Clone for ArcPtrWrapper<T> {
-    fn clone(&self) -> Self {
-        ArcPtrWrapper(self.0.clone())
     }
 }
 
