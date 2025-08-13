e05b40b918dd59df75f9bb6af23f70e4a7ee1b91
IsaacBreen <mail@isaacbreen.com>
2025-08-12 18:39:07 -0700

    fk

--- src/glr/parser.rs
+++ src/glr/parser.rs
@@ -857,6 +857,7 @@ impl<'a> GLRParserState<'a> { // No longer generic
             crate::debug!(4, "States to push after reduction: {:?}", states_to_push);
             let new_trie2_end: Arc<Mutex<PrecomputeNode2>> = Arc::new(Mutex::new(PrecomputeNode2::new(PrecomputedNodeContents::end())));
             for (k, acc_arc) in popper.below_bottom {
+                crate::debug!(4, "Processing popped below bottom for k={} with acc: {:?}", k, acc_arc);
                 let mut acc: Acc = acc_arc.as_ref().clone();
                 let active_llm_tokens = acc.union_llm_tokens();
                 let trie2_nodes = std::mem::take(&mut acc.trie2_nodes);
