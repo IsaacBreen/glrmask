5893a48c936df8aaef4e359889215fde3e4635c1
IsaacBreen <mail@isaacbreen.com>
2025-08-23 15:20:59 -0700

    f

--- src/constraint.rs
+++ src/constraint.rs
@@ -2853,10 +2853,7 @@ impl<'a> GrammarConstraintState<'a> {
         crate::debug!(2, "Active tokenizer states after committing text (bytes {:?}): {:?}", llm_token_bytes, self.state.keys().map(|k|k.0).collect::<Vec<_>>());
         for (tokenizer_id, glr_state) in &self.state {
             if !glr_state.active_state.stack.is_empty() { // Log only for non-empty GSS
-                // glr_state.log_gss(
-                //     &format!("GSS for tokenizer state {} after commit of text", tokenizer_id.0),
-                //     TerminalID(0)
-                // );
+                glr_state.log_gss("After commit", TerminalID(0), false, false);
             }
         }
     }
