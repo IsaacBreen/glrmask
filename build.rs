030e175bce2a9e64bb262e0e9768a46569633137
IsaacBreen <mail@isaacbreen.com>
2025-08-26 14:34:30 -0700

    hm

--- build.rs
+++ build.rs
@@ -3,6 +3,9 @@ use std::env;
 
 fn main() {
     if (env::var("ENABLE_PROGRESS_BAR").is_ok() || cfg!(pbar)) && !cfg!(no_pbar) {
+        // Enable progress bar
+    } else {
+        // Disable progress bar
         println!("cargo:rustc-cfg=rustrover");
     }
 
