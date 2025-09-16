b08f2c012a0b2501e4db81c77f2279bd4d1dc678
IsaacBreen <mail@isaacbreen.com>
2025-09-16 20:42:07 +0800

    hm

--- src/example_code.js
+++ src/example_code.js
@@ -1,49 +1,19 @@
-{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-if (true) {
-    if (true) {
-        if (true) {
-            if (true) {
-                if (true) {
-                    if (true) {
-                        if (true) {
-                            if (true) {
-                                if (true) {
-                                    if (true) {
-                                        if (true) {
-                                            if (true) {
-                                                if (true) {
-                                                    if (true) {
-                                                        if (true) {
-                                                            if (true) {
-                                                                if (true) {
-                                                                    if (true) {
-                                                                        if (true) {
-                                                                            if (true) {
-                                                                                if (true) {
-                                                                                    if (true) {
-                                                                                        if (true) {
-                                                                                            if (true) {
-                                                                                            }
-                                                                                        }
-                                                                                    }
-                                                                                }
-                                                                            }
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+// Variable declarations
+var a = 1;
+let b = "hello";
+const c = true;
+
+// Reassignment
+a = 2;
+b = 'world';
+// const c = false; // This should be a syntax error if your parser handles const correctly
+
+// Primitive types
+let myNumber = 123.45;
+let myBigInt = 9007199254740991n;
+let myStringSingle = 'This is a string.';
+let myStringDouble = "This is also a string.";
+let myBoolean = false;
+let myNull = null;
+let myUndefined = undefined;
+let mySymbol = Symbol('description');
