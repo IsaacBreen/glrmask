449481b45dd7aed99736ae0ec0da83d955eece66
IsaacBreen <mail@isaacbreen.com>
2025-09-17 20:47:45 +0800

    ffs

--- python/aug25/models/precompute3_model_pure_python.py
+++ python/aug25/models/precompute3_model_pure_python.py
@@ -339,7 +339,8 @@ class Model(GraphProvider):
         print("GSS at start of get_mask:")
         state_map = self.state
 
-        # state_map = {sid: convert_rust_gss_to_python_gss(rust_gss) for sid, rust_gss in self.constraint_state.get_state_map().items()}
+        expected_state_map = {sid: convert_rust_gss_to_python_gss(rust_gss) for sid, rust_gss in self.constraint_state.get_state_map().items()}
+        assert state_map == expected_state_map, f"state_map != expected_state_map: {state_map.keys() ^ expected_state_map.keys()}"
 
         all_ones_mask = self.all_internal_llm_tokens_bitset
 
