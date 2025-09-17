f121628836dd4208739e40d0bfca456aa5171cda
IsaacBreen <mail@isaacbreen.com>
2025-09-17 12:22:01 +0800

    egc

--- src/example_code.js
+++ src/example_code.js
@@ -1,19 +1,1 @@
-// Variable declarations
-var a = 1;
 let b = "hello";
-const c = true;
-
-// Reassignment
-a = 2;
-b = 'world';
-// const c = false; // This should be a syntax error if your parser handles const correctly
-
-// Primitive types
-let myNumber = 123.45;
-let myBigInt = 9007199254740991n;
-let myStringSingle = 'This is a string.';
-let myStringDouble = "This is also a string.";
-let myBoolean = false;
-let myNull = null;
-let myUndefined = undefined;
-let mySymbol = Symbol('description');
