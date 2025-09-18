bfbf049721786ec2f405d0adc9e82eb75d2a69cd
IsaacBreen <mail@isaacbreen.com>
2025-09-18 12:57:24 +0800

    test: Add tests for pop on merged stacks and popn

--- python/gss_tester/test_spec.py
+++ python/gss_tester/test_spec.py
@@ -100,3 +100,46 @@ def run_test_spec(gss_class: Type[GSS]) -> Generator[Tuple[Any, int], None, None
 
     pruned_all_gss = gss_to_prune.prune(lambda acc: acc > 100)
     yield from _yield_state(pruned_all_gss) # Should be an empty GSS
+
+    # --- Test 8: Pop operations on merged stacks ---
+    # Case 8a: Pop to a common parent, should result in one stack.
+    # This exposes a bug in ReferenceGSS which creates duplicate stacks.
+    s_base = gss_class.from_stacks([([], acc_factory())]).push(100)
+    s_branch1 = s_base.push(101)
+    s_branch2 = s_base.push(102)
+    s_merged = gss_class.merge([s_branch1, s_branch2], merge_func)
+    yield from _yield_state(s_merged) # has [100, 101] and [100, 102], both acc 0
+
+    s_popped = s_merged.pop()
+    yield from _yield_state(s_popped) # Should have one stack: [100] with acc 0
+
+    # Case 8b: Push after a pop that should have merged.
+    # This further highlights the divergence from the duplicate stacks.
+    s_pushed_after_pop = s_popped.push(103)
+    yield from _yield_state(s_pushed_after_pop) # Should have one stack: [100, 103] acc 0
+
+    # Case 8c: Pop to a common parent with different accumulators.
+    # This exposes a bug in FastGSS which loses accumulator information on pop.
+    s_base2 = gss_class.from_stacks([([], acc_factory())]).push(200)
+    s_branch3 = s_base2.push(201).apply(lambda _: 7)
+    s_branch4 = s_base2.push(202).apply(lambda _: 8)
+    s_merged2 = gss_class.merge([s_branch3, s_branch4], merge_func)
+    yield from _yield_state(s_merged2) # has [200, 201] acc 7 and [200, 202] acc 8
+
+    s_popped2 = s_merged2.pop()
+    # Should have two stacks: [200] acc 7, and [200] acc 8
+    yield from _yield_state(s_popped2)
+
+    # --- Test 9: popn ---
+    # Case 9a: popn less than stack depth
+    gss_pn1 = gss_class.from_stacks([([], 0)]).push(1).push(2).push(3)
+    gss_pn2 = gss_class.from_stacks([([], 0)]).push(4).push(5)
+    gss_pn_merged = gss_class.merge([gss_pn1, gss_pn2], merge_func)
+    yield from _yield_state(gss_pn_merged) # has [1,2,3] and [4,5]
+
+    gss_pn_pop2 = gss_pn_merged.popn(2)
+    yield from _yield_state(gss_pn_pop2) # has [1] and []
+
+    # Case 9b: popn more than stack depth
+    gss_pn_pop4 = gss_pn_merged.popn(4)
+    yield from _yield_state(gss_pn_pop4) # has []
