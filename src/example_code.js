062732bb4c95d3def8311635b602590e741d74a5
IsaacBreen <mail@isaacbreen.com>
2025-09-18 17:12:14 +0800

    f

--- src/example_code.js
+++ src/example_code.js
@@ -1,1 +1,26 @@
+aaaaaaaa
+// [x]: 1
+// [Symbol('description')]: 2
+
+let aaaaaaaa = 11111111111111111111111111111111;
+const myObject = {
+    // Numeric keys
+    123: "numeric key", // Numeric keys
+    // Computed property name
+    [x]: "symbol key value",
+    [Symbol('description')]: "symbol key value",
+};
+let aaaaaaaa = 11111111111111111111111111111111;
+let aaaaaaaa = 11111111111111111111111111111111;
+aaaaa;
+//=========
+// <></>
+
+// =================================================================
+//  JavaScript Parser Test Suite - "The Kitchen Sink"
+//  This file contains a wide variety of JS syntax constructs
+//  to test the robustness and completeness of a parser.
+// =================================================================
+
+
 if (true) { if (true) { if (true) {
