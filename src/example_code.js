5361ff812c302f65e39c7403e93999afc6203a49
IsaacBreen <mail@isaacbreen.com>
2025-09-18 16:09:05 +0800

    more egs

--- src/example_code.js
+++ src/example_code.js
@@ -1,1 +1,19 @@
-let a=
+aaaaaaaa
+// [x]: 1
+// [Symbol('description')]: 2
+
+let aaaaaaaa = 11111111111111111111111111111111;
+const myObject = {
+    // Numeric keys
+    123: "numeric key", // Numeric keys
+    // Computed property name
+    [x]: "symbol key value",
+    [Symbol('description')]: "symbol key value",
+};
+let aaaaaaaa = 11111111111111111111111111111111;
+let aaaaaaaa = 11111111111111111111111111111111;
+aaaaa;
+//=========
+// <></>
+
+// =================================================================
