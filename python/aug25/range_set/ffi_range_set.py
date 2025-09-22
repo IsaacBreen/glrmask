5e64822095e4d03097fea3772103dbfbc747b651
IsaacBreen <mail@isaacbreen.com>
2025-09-22 19:43:48 +0800

    difference

--- python/aug25/range_set/ffi_range_set.py
+++ python/aug25/range_set/ffi_range_set.py
@@ -85,7 +85,7 @@ class FFIRangeSet(RangeSet[int]):
             record_metric('FFIRangeSet.difference.in_len_b', len(other))
         except Exception:
             pass
-        res = self - other
+        res = self.difference(other)
         try:
             record_metric('FFIRangeSet.difference.out_len', len(res))
         except Exception:
