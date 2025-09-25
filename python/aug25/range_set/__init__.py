571128ce9cc78ef59f33ca3b208ef3c0d4d591fa
IsaacBreen <mail@isaacbreen.com>
2025-09-25 12:01:36 +0800

    feat: Add nodeopt_optimizer module for graph optimization

--- python/aug25/range_set/__init__.py
+++ python/aug25/range_set/__init__.py
@@ -1,9 +1,15 @@
 from .py_range_set import PyRangeSet
 from .ffi_range_set import FFIRangeSet
 from .bitset_range_set import BitsetRangeSet
+from .nodeopt_optimizer import (
+    NodeOpt,
+    UnconditionalEdge,
+    StateEdge,
+    _unconditionalize_guaranteed_transitions,
+)
 
 # RangeSet = PyRangeSet
 RangeSet = FFIRangeSet
 
-__all__ = ["RangeSet", "PyRangeSet", "FFIRangeSet", "BitsetRangeSet"]
+__all__ = ["RangeSet", "PyRangeSet", "FFIRangeSet", "BitsetRangeSet", "NodeOpt", "UnconditionalEdge", "StateEdge", "_unconditionalize_guaranteed_transitions"]
 
