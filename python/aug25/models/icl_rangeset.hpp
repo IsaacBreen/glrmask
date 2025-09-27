82607d1b0fdf9e95e1331bc29bd7e2c4396e4922
IsaacBreen <mail@isaacbreen.com>
2025-09-27 15:17:17 +0800

    NICE!!!!!!!!!! Revert "Revert "Fix: Normalize Boost.ICL intervals to inclusive bounds in RangeSet output/enumeration to correct boundary errors.""
    This reverts commit 19f410d31a0688408255f3454744cf86145626ee.

--- python/aug25/models/icl_rangeset.hpp
+++ python/aug25/models/icl_rangeset.hpp
@@ -6,7 +6,7 @@
 #include <algorithm>
 #include <boost/functional/hash.hpp>
 #include <boost/icl/interval_set.hpp>
-#include <boost/icl/closed_interval.hpp>
+#include <boost/icl/interval.hpp>
 #include "stats.hpp"
 
 class RangeSet {
@@ -87,7 +87,8 @@ public:
     std::vector<std::pair<unsigned long long, unsigned long long>> to_ranges() const {
         std::vector<std::pair<unsigned long long, unsigned long long>> ranges;
         for (const auto& interval : set) {
-            ranges.emplace_back(interval.lower(), interval.upper());
+            // boost::icl::first/last are free functions that give inclusive bounds
+            ranges.emplace_back(boost::icl::first(interval), boost::icl::last(interval));
         }
         return ranges;
     }
@@ -95,9 +96,10 @@ public:
     std::vector<unsigned long long> to_indices() const {
         std::vector<unsigned long long> indices;
         for (const auto& interval : set) {
-            for (unsigned long long i = interval.lower(); ; ++i) {
+            // boost::icl::last is inclusive for discrete intervals
+            for (unsigned long long i = boost::icl::first(interval); ; ++i) {
                 indices.push_back(i);
-                if (i == interval.upper()) break; // handle overflow for max ull
+                if (i == boost::icl::last(interval)) break; // handle overflow for max ull
             }
         }
         return indices;
@@ -115,7 +117,7 @@ public:
             if (!first) {
                 ss << ", ";
             }
-            ss << "(" << interval.lower() << ", " << interval.upper() << ")";
+            ss << "(" << boost::icl::first(interval) << ", " << boost::icl::last(interval) << ")";
             first = false;
         }
         ss << "]";
@@ -137,10 +139,6 @@ public:
         return seed;
     }
 private:
-    boost::icl::interval_set<
-        unsigned long long,
-        std::less,
-        boost::icl::closed_interval<unsigned long long>
-    > set;
+    boost::icl::interval_set<unsigned long long> set;
 };
 
