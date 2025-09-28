dcef5033170c946d08426546e901abcd9f995580
IsaacBreen <mail@isaacbreen.com>
2025-09-28 14:47:39 +0800

    Revert "add new rangeset"
    This reverts commit 909d26f5845160ede8cecd8a7403be4b5fd8dad6.

--- python/aug25/range_set/__init__.py
+++ python/aug25/range_set/__init__.py
@@ -1,11 +1,9 @@
 from .py_range_set import PyRangeSet
 from .ffi_range_set import FFIRangeSet
 from .bitset_range_set import BitsetRangeSet
-from .fast_py_range_set import FastPyRangeSet
 
 # RangeSet = PyRangeSet
-# RangeSet = FFIRangeSet
-RangeSet = FastPyRangeSet
+RangeSet = FFIRangeSet
 
-__all__ = ["RangeSet", "PyRangeSet", "FFIRangeSet", "BitsetRangeSet", "FastPyRangeSet"]
+__all__ = ["RangeSet", "PyRangeSet", "FFIRangeSet", "BitsetRangeSet"]
 
