8c412cfc4997a5dff025a1395bed44895ced4c52
IsaacBreen <mail@isaacbreen.com>
2025-10-02 18:28:18 +0800

    fix!!

--- python/aug25/models/precompute3_model_pure_python.py
+++ python/aug25/models/precompute3_model_pure_python.py
@@ -412,9 +412,8 @@ class Model(GraphProvider):
         # Load tokenizer DFA from JSON
         dfa_data = data['tokenizer']['dfa']
         dfa_states = []
-        print("data['tokenizer']:")
-        print(data['tokenizer'])
         for state_data in dfa_data['states']:
+            print(state_data)
             transitions_json = state_data['transitions']
             # The 'data' field of the TrieMap JSON contains string keys for byte values.
             transitions = {int(k): v for k, v in transitions_json.get('data', {}).items()}
@@ -422,8 +421,15 @@ class Model(GraphProvider):
             group_id_to_u8set_json = state_data.get('group_id_to_u8set', [])
             group_id_to_u8set = {}
             for group_id, u8set_ranges in group_id_to_u8set_json:
-                # The u8set is stored as a list of [start, end] ranges.
-                group_id_to_u8set[group_id] = TerminalIdSet.from_ranges(u8set_ranges)
+                u8set_ranges2 = []
+                for r in u8set_ranges:
+                    if isinstance(r, list):
+                        u8set_ranges2.append(r)
+                    elif isinstance(r, int):
+                        u8set_ranges2.append([r, r])
+                    else:
+                        raise ValueError(f"Invalid range value: {r}")
+                group_id_to_u8set[group_id] = TerminalIdSet.from_ranges(u8set_ranges2)
 
             dfa_states.append(DFAState(
                 transitions=transitions,
