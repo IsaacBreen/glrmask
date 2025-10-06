9cf383e3455f8c841c6a8188862ec774815c23df
IsaacBreen <mail@isaacbreen.com>
2025-10-06 20:00:14 +0800

    run thrice

--- src/constraint.rs
+++ src/constraint.rs
@@ -972,6 +972,8 @@ impl GrammarConstraint {
 
         helper.run_dfs();
         helper.optimize();
+        helper.optimize();
+        helper.optimize();
         let (precomputed0, trie0_god) = helper.finish();
         let roots: Vec<_> = precomputed0.values().cloned().collect();
         Self::has_llm_compatible_cycle0(&trie0_god, &roots, internal_max_llm_token);
@@ -1462,6 +1464,8 @@ impl GrammarConstraint {
 
         crate::debug!(2, "Finished precomputing Trie 3.");
         let max_state_id = parser.table.keys().map(|s| s.0).max().unwrap_or(0);
+        optimize_trie3_size(&mut precomputed3, &trie3_god, config, max_state_id, internal_max_llm_token, stage_vocab);
+        optimize_trie3_size(&mut precomputed3, &trie3_god, config, max_state_id, internal_max_llm_token, stage_vocab);
         optimize_trie3_size(&mut precomputed3, &trie3_god, config, max_state_id, internal_max_llm_token, stage_vocab);
 
         (precomputed3, trie3_god)
