140fbac700957aa2ee9de5f3f791d5fb36dea721
IsaacBreen <mail@isaacbreen.com>
2025-10-07 12:01:12 +0800

    ok. js length 1

--- src/test_constraint_js.rs
+++ src/test_constraint_js.rs
@@ -513,7 +513,7 @@ fn test_js_constraint_integration() -> Result<(), Box<dyn std::error::Error>> {
 
     // 1. Load and compile the JavaScript grammar.
     // let grammar_path = "src/js_simplified_minimal_if_stmt_ambiguity_linear_time.ebnf";
-    let grammar_path = "src/js_simplified3.ebnf";
+    let grammar_path = "src/js.ebnf";
     let grammar_definition = GrammarDefinition::from_ebnf_file(grammar_path)?;
     println!("Compiling GrammarDefinition into CompiledGrammar...");
     let compiled_grammar = CompiledGrammar::from_definition(Arc::new(grammar_definition));
@@ -565,11 +565,11 @@ fn test_js_constraint_integration() -> Result<(), Box<dyn std::error::Error>> {
         // gpt2_raw_vocab.retain(|s| { s.len() == 1 || s.contains('-') });
 
         // Option 3: Set to a few specific tokens
-        gpt2_raw_vocab = vec![
-            // "a",
-            " ",
-            "@",
-        ].iter().map(|s| s.to_string()).collect();
+        // gpt2_raw_vocab = vec![
+        //     // "a",
+        //     " ",
+        //     "@",
+        // ].iter().map(|s| s.to_string()).collect();
 
 
         println!("  - Set to a specific small set of tokens: {} tokens.", gpt2_raw_vocab.len());
