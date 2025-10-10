cfd8b03a180e5b3c9f36e1122c2e431d27104f27
IsaacBreen <mail@isaacbreen.com>
2025-10-10 10:46:14 +0800

    f

--- src/constraint.rs
+++ src/constraint.rs
@@ -1691,22 +1691,24 @@ impl GrammarConstraint {
         let mut precomputed3 = BTreeMap::new();
         let trie3_god = Trie3GodWrapper::new();
 
-        for tid in tqdm!(0..=tokenizer.num_groups() as usize, desc = "Trie 3 temp", disable = !PROGRESS_BAR_ENABLED, leave=false) {
-            let parser = parser.unwrap();
-            let terminal = TerminalID(tid);
+        if false {
+            for tid in tqdm!(0..=tokenizer.num_groups() as usize, desc = "Trie 3 temp", disable = !PROGRESS_BAR_ENABLED, leave=false) {
+                let parser = parser.unwrap();
+                let terminal = TerminalID(tid);
 
-            let trie3_root = PrecomputeNode3Index::new(trie3_god.insert(PrecomputeNode3::new(PrecomputedNodeContents::root(internal_max_llm_token))));
-            let mut acc = Acc::new_fresh();
-            acc.stored_trie_nodes_mut().insert(trie3_root); // TEMP
-            let gss_leaf = Arc::new(GSSNode::new(acc));
+                let trie3_root = PrecomputeNode3Index::new(trie3_god.insert(PrecomputeNode3::new(PrecomputedNodeContents::root(internal_max_llm_token))));
+                let mut acc = Acc::new_fresh();
+                acc.stored_trie_nodes_mut().insert(trie3_root); // TEMP
+                let gss_leaf = Arc::new(GSSNode::new(acc));
 
-            let gss_stack = Arc::new(gss_leaf.push(ParseStateEdgeContent { state_id: parser.hallucinated_state_id }));
+                let gss_stack = Arc::new(gss_leaf.push(ParseStateEdgeContent { state_id: parser.hallucinated_state_id }));
 
-            let mut glr_state = parser.init_glr_parser_from_stack(gss_stack).with_god(trie3_god.clone());
-            glr_state.process_token_advanced(terminal, &ProcessTokenAdvancedConfig { below_bottom_mode: BELOW_BOTTOM_REDUCE_MODE });
+                let mut glr_state = parser.init_glr_parser_from_stack(gss_stack).with_god(trie3_god.clone());
+                glr_state.process_token_advanced(terminal, &ProcessTokenAdvancedConfig { below_bottom_mode: BELOW_BOTTOM_REDUCE_MODE });
+            }
+
+            return (precomputed3, trie3_god); // TEMP
         }
-
-        return (precomputed3, trie3_god); // TEMP
 
         let parser = parser.unwrap();
         let mut initial_values_for_map: Vec<(PrecomputeNode1Index, GLRParserState)> = Vec::new();
