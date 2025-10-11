e9db3559b1740dd05b966f08fd71f94be9814625
IsaacBreen <mail@isaacbreen.com>
2025-10-11 11:12:49 +0800

    hmmmmmmmm

--- src/test_constraint_basic.rs
+++ src/test_constraint_basic.rs
@@ -1658,48 +1658,11 @@ fn test_gss_structural_sharing_factor2() -> Result<(), Box<dyn std::error::Error
             'p01' VALUE POST
           | 'p02' VALUE POST
           | 'p03' VALUE POST
-          | 'p04' VALUE POST
-          | 'p05' VALUE POST
-          | 'p06' VALUE POST
-          | 'p07' VALUE POST
-          | 'p08' VALUE POST
-          | 'p09' VALUE POST
-          | 'p10' VALUE POST
-          | 'p11' VALUE POST
-          | 'p12' VALUE POST
-          | 'p13' VALUE POST
-          | 'p14' VALUE POST
-          | 'p15' VALUE POST
-          | 'p16' VALUE POST
-          | 'p17' VALUE POST
-          | 'p18' VALUE POST
-          | 'p19' VALUE POST
-          | 'p20' VALUE POST
-          | 'p21' VALUE POST
-          | 'p22' VALUE POST
-          | 'p23' VALUE POST
-          | 'p24' VALUE POST
-          | 'p25' VALUE POST
-          | 'p26' VALUE POST
-          | 'p27' VALUE POST
-          | 'p28' VALUE POST
-          | 'p29' VALUE POST
-          | 'p30' VALUE POST
-          | 'p31' VALUE POST
-          | 'p32' VALUE POST
         ;
 
         VALUE ::= IDENTIFIER;
 
-        POST ::=
-            't01' | 't02' | 't03' | 't04' | 't05' | 't06' | 't07' | 't08'
-          | 't09' | 't10' | 't11' | 't12' | 't13' | 't14' | 't15' | 't16'
-          | 't17' | 't18' | 't19' | 't20' | 't21' | 't22' | 't23' | 't24'
-          | 't25' | 't26' | 't27' | 't28' | 't29' | 't30' | 't31' | 't32'
-          | 't33' | 't34' | 't35' | 't36' | 't37' | 't38' | 't39' | 't40'
-          | 't41' | 't42' | 't43' | 't44' | 't45' | 't46' | 't47' | 't48'
-          | 't49' | 't50'
-        ;
+        POST ::= 't01' | 't02' | 't03';
 
         EOF ::= '$';
 
