3c2614da8faa317d48c7d69f7b222f483182cb21
IsaacBreen <mail@isaacbreen.com>
2025-10-11 09:47:00 +0800

    HMF

--- src/constraint.rs
+++ src/constraint.rs
@@ -1692,8 +1692,9 @@ impl GrammarConstraint {
         let trie3_god = Trie3GodWrapper::new();
 
         if true {
-            let tid = 3;
-            // for tid in tqdm!(0..=tokenizer.num_groups() as usize, desc = "Trie 3 temp", disable = !PROGRESS_BAR_ENABLED, leave=false) {
+            // let tid = 3;
+            for tid in tqdm!(0..=tokenizer.num_groups() as usize, desc = "Trie 3 temp", disable = !PROGRESS_BAR_ENABLED, leave=false) {
+                println!("--- Testing token ID {} ---", tid);
                 let parser = parser.unwrap();
                 let terminal = TerminalID(tid);
 
@@ -1707,9 +1708,9 @@ impl GrammarConstraint {
                 let mut glr_state = parser.init_glr_parser_from_stack(gss_stack).with_god(trie3_god.clone());
                 glr_state.process_token_advanced(terminal, &ProcessTokenAdvancedConfig { below_bottom_mode: BELOW_BOTTOM_REDUCE_MODE });
                 println!("stats: {:?}", glr_state.active_state.stack.inner.stats());
-                println!("GLR state stacks: {}", glr_state);
-                println!("GLR state graph structure:{}", glr_state.active_state.stack.inner.to_graph_string(false));
-            // }
+                // println!("GLR state stacks: {}", glr_state);
+                // println!("GLR state graph structure:{}", glr_state.active_state.stack.inner.to_graph_string(false));
+            }
 
             return (precomputed3, trie3_god); // TEMP
         }
