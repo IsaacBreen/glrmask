0c6af8c14a61141111aed78496d6de92b87bace1
IsaacBreen <mail@isaacbreen.com>
2025-10-12 18:30:49 +0800

    frick

--- src/glr/parser.rs
+++ src/glr/parser.rs
@@ -1960,6 +1960,20 @@ impl<'a> GLRParserState<'a> { // No longer generic
                                 // Update local cache with the imported destination
                                 let dest_entry = self.below_bottom_cache.entry(cache_key).or_insert(new_root.clone());
                                 *dest_entry = new_root.clone();
+                                // Link the existing trie nodes to the cached root
+                                let existing_nodes = acc.stored_trie_nodes().clone();
+                                for existing_node in existing_nodes {
+                                    let inserter = EdgeInserter::new(
+                                        dest_god,
+                                        existing_node.as_arc().clone(),
+                                        edge_key_all_tokens_zero_k.clone(),
+                                        StateIDBV::max_ones(),
+                                        |e, n| *e |= n,
+                                        |node_value, _edge_value| node_value.live_tokens |= &tokens_all,
+                                        |_, _| {}, // Unconditional insertion
+                                    );
+                                    inserter.try_destination(new_root.clone()).expect("Cycle detected when adding precompute trie edges for cached below-bottom");
+                                }
                                 // Map trie node indices within the stored GSS to local arena indices
                                 let mut mapped_gss = stored_gss.clone();
                                 map_trie3_node_ids(&mut mapped_gss, &id_map);
