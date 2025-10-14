4ffabcc5a4a8de904cdb8b381fe8c31f8aeb7a24
IsaacBreen <mail@isaacbreen.com>
2025-10-14 20:41:37 +0800

    fk

--- src/test_constraint_basic.rs
+++ src/test_constraint_basic.rs
@@ -78,6 +78,7 @@ fn test_trivial() {
     constraint.dump_precomputed1();
     // constraint.dump_precomputed2();
     constraint.dump_precomputed3();
+    return;
 
     println!("Initializing constraint state...");
     let mut state = constraint.init();
