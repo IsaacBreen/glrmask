cac607b99686e46ac8db290aa926d06ef43999f4
IsaacBreen <mail@isaacbreen.com>
2025-10-20 13:56:34 +0800

    Fix: Correct type mismatches in clone_trie3_graph after Arena method signature changes

--- src/constraint_precompute3_utils.rs
+++ src/constraint_precompute3_utils.rs
@@ -27,7 +27,7 @@ pub fn clone_trie3_graph(
     let mut q: VecDeque<IntermediatePrecomputeNode3Index> = VecDeque::new();
 
     let root_ptr = *root;
-    let root_value = trie3_god.with(root_ptr, |n| n.value.clone()).expect("root must exist");
+    let root_value = trie3_god.with(root_ptr.into(), |n| n.value.clone()).expect("root must exist");
     let new_root = IntermediatePrecomputeNode3Index::new(trie3_god.insert(Trie::new(root_value)));
     map.insert(root_ptr, new_root);
     q.push_back(root_ptr);
@@ -37,7 +37,7 @@ pub fn clone_trie3_graph(
         let new_arc = map.get(&old_ptr).expect("parent must be created").clone();
 
         let children_snapshot: Vec<( IntermediateTrie3EdgeKey, Vec<(IntermediatePrecomputeNode3Index, ())> )> = {
-            trie3_god.with(old_arc, |g| g.children()
+            trie3_god.with(old_arc.into(), |g| g.children()
                 .iter()
                 .map(|(ek, dest_map)| {
                     let entries = dest_map
@@ -55,7 +55,7 @@ pub fn clone_trie3_graph(
             for (node_ptr, _ev) in entries {
                 let child_ptr_old = *node_ptr;
                 if !map.contains_key(&child_ptr_old) {
-                    let child_value = trie3_god.with(child_ptr_old, |n| n.value.clone()).expect("child must exist");
+                    let child_value = trie3_god.with(child_ptr_old.into(), |n| n.value.clone()).expect("child must exist");
                     let child_arc_new = IntermediatePrecomputeNode3Index::new(trie3_god.insert(Trie::new(child_value)));
                     map.insert(child_ptr_old, child_arc_new);
                     q.push_back(child_ptr_old);
@@ -64,18 +64,18 @@ pub fn clone_trie3_graph(
         }
 
         {
-            trie3_god.with_mut(new_arc, |new_g| {
+            trie3_god.with_mut(new_arc.into(), |new_g| {
                 for (ek, entries) in children_snapshot {
                     for (old_node_ptr, ev) in entries {
-                        let new_key = *map.get(old_node_ptr).expect("must exist");
-                        new_g.children_mut().entry(ek).or_default().insert(new_key, ev);
+                        let new_key = *map.get(&old_node_ptr).expect("must exist");
+                        new_g.children_mut().entry(ek.clone()).or_default().insert(new_key, ev);
                     }
                 }
             }).expect("new_arc must exist");
         }
     }
 
-    Trie::recompute_all_max_depths(trie3_god, &[new_root.clone()]);
+    Trie::recompute_all_max_depths(trie3_god, &[new_root]);
     (new_root, map)
 }
 
