6c036a1c3ec810e41fed54402f35784e4c2a3eed
IsaacBreen <mail@isaacbreen.com>
2025-10-22 10:07:45 +0800

    print gc here

--- src/bin/grammar_compiler.rs
+++ src/bin/grammar_compiler.rs
@@ -115,6 +115,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         &config,
         loaded_pc0,
     );
+    grammar_constraint.dump_precomputed1();
+    grammar_constraint.dump_precomputed3();
+
     println!("GrammarConstraint constructed successfully.");
     if let Some(path) = args.save_precompute0.as_ref() {
         println!("Saving precompute0 cache to: {:?}", path);
