d4bc4bf98a754c7f05bbbdf94878e509b30691ae
IsaacBreen <mail@isaacbreen.com>
2025-10-25 20:56:46 +0800

    formatting

--- src/constraint_special_precompute.rs
+++ src/constraint_special_precompute.rs
@@ -1,7 +1,7 @@
 use std::collections::{BTreeSet, HashSet, VecDeque};
 
 use crate::constraint::{
-    GrammarConstraint, GrammarConstraintState, LLMTokenBV, PrecomputeNode1Index
+    GrammarConstraint, GrammarConstraintState, LLMTokenBV, PrecomputeNode1Index,
 };
 use crate::glr::parser::GLRParser;
 use crate::glr::table::{Goto, NonTerminalID, Stage7ShiftsAndReducesLookaheadValue, StateID};
@@ -9,104 +9,111 @@ use crate::types::TerminalID;
 
 // Types for special precomputation
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
-    pub enum SpecialPrecomputeDest {
-        Reduce { pop: usize, dest_nt: NonTerminalID },
-        Escape { push_states: Vec<StateID> },
-    }
+pub enum SpecialPrecomputeDest {
+    Reduce { pop: usize, dest_nt: NonTerminalID },
+    Escape { push_states: Vec<StateID> },
+}
 
-    // (Option<NonTerminalID>, StateID, TerminalID, SpecialPrecomputeDest)
-    pub type SpecialPrecomputeNormalEdge =
-        (Option<NonTerminalID>, StateID, TerminalID, SpecialPrecomputeDest);
+// (Option<NonTerminalID>, StateID, TerminalID, SpecialPrecomputeDest)
+pub type SpecialPrecomputeNormalEdge = (
+    Option<NonTerminalID>,
+    StateID,
+    TerminalID,
+    SpecialPrecomputeDest,
+);
 
-    // (Option<NonTerminalID>, TerminalID, (usize, NonTerminalID), LLMTokenBV, PrecomputeNode1Index, PrecomputeNode1Index)
-    pub type SpecialPrecomputeSuperEdge = (
-        Option<NonTerminalID>,
-        TerminalID,
-        (usize, NonTerminalID),
-        LLMTokenBV,
-        PrecomputeNode1Index,
-        PrecomputeNode1Index,
-    );
+// (Option<NonTerminalID>, TerminalID, (usize, NonTerminalID), LLMTokenBV, PrecomputeNode1Index, PrecomputeNode1Index)
+pub type SpecialPrecomputeSuperEdge = (
+    Option<NonTerminalID>,
+    TerminalID,
+    (usize, NonTerminalID),
+    LLMTokenBV,
+    PrecomputeNode1Index,
+    PrecomputeNode1Index,
+);
 
-    #[derive(Debug, Clone, Default, PartialEq, Eq)]
-    pub struct SpecialPrecomputation {
-        pub normal_edges: HashSet<SpecialPrecomputeNormalEdge>,
-        pub super_edges: HashSet<SpecialPrecomputeSuperEdge>,
-    }
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
+pub struct SpecialPrecomputation {
+    pub normal_edges: HashSet<SpecialPrecomputeNormalEdge>,
+    pub super_edges: HashSet<SpecialPrecomputeSuperEdge>,
+}
 
-    // Helper to get actions for a state and terminal
-    fn get_actions<'a>(
-        parser: &'a GLRParser,
-        state_id: StateID,
-        terminal_id: TerminalID,
-    ) -> Vec<&'a Stage7ShiftsAndReducesLookaheadValue> {
-        let mut actions = Vec::new();
-        if let Some(row) = parser.table.get(&state_id) {
-            if let Some(action) = row.shifts_and_reduces_full.get(&terminal_id) {
-                actions.push(action);
-            }
-        }
-        actions
-    }
-
-    // Helper to get gotos for a state and non-terminal
-    fn get_gotos<'a>(parser: &'a GLRParser, state_id: StateID, nt_id: NonTerminalID) -> Vec<&'a Goto> {
-        if let Some(row) = parser.table.get(&state_id) {
-            row.gotos.get(&nt_id).map(|g| vec![g]).unwrap_or_default()
-        } else {
-            vec![]
+// Helper to get actions for a state and terminal
+fn get_actions<'a>(
+    parser: &'a GLRParser,
+    state_id: StateID,
+    terminal_id: TerminalID,
+) -> Vec<&'a Stage7ShiftsAndReducesLookaheadValue> {
+    let mut actions = Vec::new();
+    if let Some(row) = parser.table.get(&state_id) {
+        if let Some(action) = row.shifts_and_reduces_full.get(&terminal_id) {
+            actions.push(action);
         }
     }
+    actions
+}
 
-    pub fn precompute_special(gc: &GrammarConstraint) -> SpecialPrecomputation {
-        let mut normal_edges = HashSet::new();
-        let parser = &gc.parser;
+// Helper to get gotos for a state and non-terminal
+fn get_gotos<'a>(parser: &'a GLRParser, state_id: StateID, nt_id: NonTerminalID) -> Vec<&'a Goto> {
+    if let Some(row) = parser.table.get(&state_id) {
+        row.gotos.get(&nt_id).map(|g| vec![g]).unwrap_or_default()
+    } else {
+        vec![]
+    }
+}
 
-        let mut non_terminals: Vec<Option<NonTerminalID>> =
-            parser.non_terminal_map.right_values().copied().map(Some).collect();
-        non_terminals.push(None);
+pub fn precompute_special(gc: &GrammarConstraint) -> SpecialPrecomputation {
+    let mut normal_edges = HashSet::new();
+    let parser = &gc.parser;
 
-        let terminals: Vec<TerminalID> = parser.terminal_map.right_values().copied().collect();
+    let mut non_terminals: Vec<Option<NonTerminalID>> = parser
+        .non_terminal_map
+        .right_values()
+        .copied()
+        .map(Some)
+        .collect();
+    non_terminals.push(None);
 
-        let mut states: Vec<StateID> = parser.table.keys().copied().collect();
-        states.sort();
-        states.dedup();
+    let terminals: Vec<TerminalID> = parser.terminal_map.right_values().copied().collect();
 
-        for src_nt in &non_terminals {
-            for &initial_state in &states {
-                for &terminal in &terminals {
-                    let mut q: VecDeque<(Vec<StateID>, Vec<StateID>)> = VecDeque::new(); // (stack, pushed_path)
-                    let mut visited_stacks = HashSet::new();
+    let mut states: Vec<StateID> = parser.table.keys().copied().collect();
+    states.sort();
+    states.dedup();
 
-                    let initial_stacks = if let Some(nt) = src_nt {
-                        let gotos = get_gotos(parser, initial_state, *nt);
-                        let mut stacks = vec![];
-                        for goto in gotos {
-                            if let Some(goto_state) = goto.state_id {
-                                stacks.push((vec![initial_state, goto_state], vec![goto_state]));
-                            }
-                        }
-                        stacks
-                    } else {
-                        vec![(vec![initial_state], vec![])]
-                    };
+    for src_nt in &non_terminals {
+        for &initial_state in &states {
+            for &terminal in &terminals {
+                let mut q: VecDeque<(Vec<StateID>, Vec<StateID>)> = VecDeque::new(); // (stack, pushed_path)
+                let mut visited_stacks = HashSet::new();
 
-                    for (stack, path) in initial_stacks {
-                        if visited_stacks.insert(stack.clone()) {
-                            q.push_back((stack, path));
+                let initial_stacks = if let Some(nt) = src_nt {
+                    let gotos = get_gotos(parser, initial_state, *nt);
+                    let mut stacks = vec![];
+                    for goto in gotos {
+                        if let Some(goto_state) = goto.state_id {
+                            stacks.push((vec![initial_state, goto_state], vec![goto_state]));
                         }
                     }
+                    stacks
+                } else {
+                    vec![(vec![initial_state], vec![])]
+                };
 
-                    while let Some((stack, pushed_path)) = q.pop_front() {
-                        let top_state = *stack.last().unwrap();
-                        let actions = get_actions(parser, top_state, terminal);
+                for (stack, path) in initial_stacks {
+                    if visited_stacks.insert(stack.clone()) {
+                        q.push_back((stack, path));
+                    }
+                }
 
-                        for action in actions {
-                            let mut handle_reduce = |
-                                normal_edges: &mut HashSet<SpecialPrecomputeNormalEdge>,
-                                len: usize,
-                                reduce_nt: NonTerminalID,
-                            | {
+                while let Some((stack, pushed_path)) = q.pop_front() {
+                    let top_state = *stack.last().unwrap();
+                    let actions = get_actions(parser, top_state, terminal);
+
+                    for action in actions {
+                        let mut handle_reduce =
+                            |normal_edges: &mut HashSet<SpecialPrecomputeNormalEdge>,
+                             len: usize,
+                             reduce_nt: NonTerminalID| {
                                 if stack.len() <= len {
                                     let pop_below = len - stack.len();
                                     let dest = SpecialPrecomputeDest::Reduce {
@@ -133,8 +140,24 @@ use crate::types::TerminalID;
                                 }
                             };
 
-                            match action {
-                                Stage7ShiftsAndReducesLookaheadValue::Shift(next_state) => {
+                        match action {
+                            Stage7ShiftsAndReducesLookaheadValue::Shift(next_state) => {
+                                let mut new_pushed = pushed_path.clone();
+                                new_pushed.push(*next_state);
+                                let dest = SpecialPrecomputeDest::Escape {
+                                    push_states: new_pushed,
+                                };
+                                normal_edges.insert((*src_nt, initial_state, terminal, dest));
+                            }
+                            Stage7ShiftsAndReducesLookaheadValue::Reduce {
+                                nonterminal_id,
+                                len,
+                                ..
+                            } => {
+                                handle_reduce(&mut normal_edges, *len, *nonterminal_id);
+                            }
+                            Stage7ShiftsAndReducesLookaheadValue::Split { shift, reduces } => {
+                                if let Some(next_state) = shift {
                                     let mut new_pushed = pushed_path.clone();
                                     new_pushed.push(*next_state);
                                     let dest = SpecialPrecomputeDest::Escape {
@@ -142,26 +165,9 @@ use crate::types::TerminalID;
                                     };
                                     normal_edges.insert((*src_nt, initial_state, terminal, dest));
                                 }
-                                Stage7ShiftsAndReducesLookaheadValue::Reduce {
-                                    nonterminal_id,
-                                    len,
-                                    ..
-                                } => {
-                                    handle_reduce(&mut normal_edges, *len, *nonterminal_id);
-                                }
-                                Stage7ShiftsAndReducesLookaheadValue::Split { shift, reduces } => {
-                                    if let Some(next_state) = shift {
-                                        let mut new_pushed = pushed_path.clone();
-                                        new_pushed.push(*next_state);
-                                        let dest = SpecialPrecomputeDest::Escape {
-                                            push_states: new_pushed,
-                                        };
-                                        normal_edges.insert((*src_nt, initial_state, terminal, dest));
-                                    }
-                                    for (len, nts) in reduces {
-                                        for (nt, _) in nts {
-                                            handle_reduce(&mut normal_edges, *len, *nt);
-                                        }
+                                for (len, nts) in reduces {
+                                    for (nt, _) in nts {
+                                        handle_reduce(&mut normal_edges, *len, *nt);
                                     }
                                 }
                             }
@@ -170,18 +176,17 @@ use crate::types::TerminalID;
                 }
             }
         }
-
-        SpecialPrecomputation {
-            normal_edges,
-            super_edges: HashSet::new(),
-        }
     }
 
-    pub fn get_mask4(gcs: &GrammarConstraintState) -> LLMTokenBV {
-        // Minimal, safe placeholder: return an empty/default mask.
-        // This avoids panics during execution until the super-edge integration is wired in.
-        LLMTokenBV::default()
+    SpecialPrecomputation {
+        normal_edges,
+        super_edges: HashSet::new(),
     }
+}
+
+pub fn get_mask4(gcs: &GrammarConstraintState) -> LLMTokenBV {
+    todo!()
+}
 
 pub fn dump_precomputed_special(gc: &GrammarConstraint) {
     let sp = &gc.special_precomputation;
@@ -191,7 +196,8 @@ pub fn dump_precomputed_special(gc: &GrammarConstraint) {
 
     // For resolving NT IDs to names
     let get_nt_name = |nt_id: &NonTerminalID| -> String {
-        parser.non_terminal_map
+        parser
+            .non_terminal_map
             .get_by_right(nt_id)
             .map(|nt| nt.to_string())
             .unwrap_or_else(|| format!("NT({})", nt_id.0))
@@ -205,7 +211,8 @@ pub fn dump_precomputed_special(gc: &GrammarConstraint) {
 
     // For resolving Terminal IDs to names
     let get_term_name = |term_id: &TerminalID| -> String {
-        parser.terminal_map
+        parser
+            .terminal_map
             .get_by_right(term_id)
             .map(|t| t.to_string())
             .unwrap_or_else(|| format!("T({})", term_id.0))
