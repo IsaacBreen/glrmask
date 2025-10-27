ca6a4d0329350156376b8255d9bd259da6357255
IsaacBreen <mail@isaacbreen.com>
2025-10-27 17:51:58 +0800

    revert

--- src/trie3_opt/passes/mod.rs
+++ src/trie3_opt/passes/mod.rs
@@ -7,21 +7,17 @@ pub trait OptimizationPass {
     fn run(&self, trie: &mut MiniTrie, ctx: &mut OptimizationContext);
 }
 
-pub mod prune_unproductive_paths;
+pub mod prune_dead_paths;
 pub mod compress_edges;
 pub mod merge_structural;
 pub mod eliminate_pop0;
 pub mod canonicalize_end_nodes;
 pub mod factor_state_fanout;
-pub mod prune_dead_paths;
-pub mod compress_chains;
 pub mod full;
 
-pub use prune_unproductive_paths::PruneUnproductivePathsPass;
+pub use prune_dead_paths::PruneDeadPathsPass;
 pub use compress_edges::CompressEdgesPass;
 pub use merge_structural::MergeStructuralPass;
 pub use eliminate_pop0::EliminatePop0ExceptRootsPass;
 pub use canonicalize_end_nodes::CanonicalizeEndNodesPass;
 pub use factor_state_fanout::FactorStateFanoutPass;
-pub use prune_dead_paths::PruneDeadPathsPass;
-pub use compress_chains::CompressChainsPass;
