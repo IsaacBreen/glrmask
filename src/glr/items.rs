b19a4308a56318ece9c720ce338f66c435a493bc
IsaacBreen <mail@isaacbreen.com>
2025-10-28 14:09:36 +0800

    okay..

--- src/glr/items.rs
+++ src/glr/items.rs
@@ -97,6 +97,7 @@ pub enum LRMode {
     LR0,
 }
 
-pub const LR_MODE: LRMode = LRMode::LR0;
+pub const LR_MODE: LRMode = LRMode::LALR;
 // pub const LR_MODE: LRMode = LRMode::LALR_EX_GOTO;
 // pub const LR_MODE: LRMode = LRMode::LR1;
+// pub const LR_MODE: LRMode = LRMode::LR0;
