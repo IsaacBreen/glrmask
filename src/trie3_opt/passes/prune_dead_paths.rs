e447fa888624786c9cabbe5ca01460960128d951
IsaacBreen <mail@isaacbreen.com>
2025-10-29 12:32:33 +0800

    try fix

--- src/trie3_opt/passes/prune_dead_paths.rs
+++ src/trie3_opt/passes/prune_dead_paths.rs
@@ -14,28 +14,26 @@ impl OptimizationPass for PruneDeadPathsPass {
         "PruneDeadPaths"
     }
 
-    fn run(&self, trie: &mut MiniTrie, ctx: &mut OptimizationContext) {
-        let universe = SortedSet::from_iter(0..=ctx.max_llm_token_id);
-        let productive_tokens = trie.productive_tokens_at_nodes(&universe);
+    fn run(&self, trie: &mut MiniTrie, _ctx: &mut OptimizationContext) {
+        // Identify nodes that can reach an END node.
+        let productive_nodes = trie.can_reach_end();
 
         let node_ids: Vec<_> = trie.node_ids().collect();
         for node_id in node_ids {
             let node = trie.get_node(node_id).unwrap();
             let mut new_children: BTreeMap<EdgeKey, BTreeMap<NodeId, SortedSet>> = BTreeMap::new();
             for (ek, dm) in node.children() {
+                // Keep the original edge key; only filter out dead destinations.
+                let mut dm2: BTreeMap<NodeId, SortedSet> = BTreeMap::new();
                 for (dst, sids) in dm {
-                    let productive_from_child = productive_tokens.get(dst).unwrap();
-                    let productive_on_edge = ek.tokens.intersect(productive_from_child);
-
-                    if !productive_on_edge.is_empty() {
-                        let new_ek = EdgeKey::new(ek.pop, productive_on_edge);
-                        new_children
-                            .entry(new_ek)
+                    if productive_nodes.contains(dst) {
+                        dm2.entry(*dst)
                             .or_default()
-                            .entry(*dst)
-                            .and_modify(|e: &mut SortedSet| e.union_inplace(sids))
-                            .or_insert(sids.clone());
+                            .union_inplace(sids);
                     }
+                }
+                if !dm2.is_empty() {
+                    new_children.insert(ek.clone(), dm2);
                 }
             }
             trie.set_children(node_id, new_children);
