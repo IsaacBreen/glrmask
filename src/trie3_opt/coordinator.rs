302281b87a01e7c0a5f644ef591fd499f683ea9e
IsaacBreen <mail@isaacbreen.com>
2025-10-30 19:03:02 +0800

    2 passes

--- src/trie3_opt/coordinator.rs
+++ src/trie3_opt/coordinator.rs
@@ -53,7 +53,7 @@ pub struct CoordinatorConfig {
 
 fn get_default_config_impl() -> CoordinatorConfig {
     CoordinatorConfig {
-        num_passes: 1,
+        num_passes: 2,
         prune_dead_paths: true,
         prune_unproductive_paths: true,
         canonicalize_end_nodes: false,
