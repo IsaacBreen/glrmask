b3a1b62900d8e289515a8ecfca327303ea8637fc
IsaacBreen <mail@isaacbreen.com>
2025-11-06 18:59:43 +0800

    simplify

--- src/precompute4/full_dwa.rs
+++ src/precompute4/full_dwa.rs
@@ -316,7 +316,9 @@ pub fn precompute4(parser: &GLRParser, precomputed1: &BTreeMap<TokenizerStateID,
 
     let now = Instant::now();
     crate::debug!(5, "Starting resolve_negative_codes_in_nwa...");
+    combined_nwa.simplify();
     resolve_negative_codes_in_nwa(&mut combined_nwa);
+    combined_nwa.simplify();
     crate::debug!(4, "resolve_negative_codes_in_nwa took: {:?}. NWA now has {} states.", now.elapsed(), combined_nwa.states.len());
 
     let now = Instant::now();
