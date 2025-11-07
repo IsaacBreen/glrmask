651bff9339cf637b79a64da54f23f41e29bbd2ff
IsaacBreen <mail@isaacbreen.com>
2025-11-07 15:45:14 +0800

    fiiiiiiirj

--- src/test_constraint_basic.rs
+++ src/test_constraint_basic.rs
@@ -1635,6 +1635,7 @@ fn test_ebnf_ignore_directive_with_partial_match() -> Result<(), Box<dyn std::er
     println!("Parser: {}", constraint.parser);
     constraint.dump_precomputed1();
     constraint.dump_precomputed3();
+    constraint.dump_precomputed4();
 
     // 5. Initialize state and get the initial mask
     let mut state = constraint.init();
