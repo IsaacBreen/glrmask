85fd410102dd9e597d1d6630b1e39eb101d231e9
IsaacBreen <mail@isaacbreen.com>
2025-11-10 15:34:39 +0800

    Revert "Refactor: Inline product automaton construction logic into NWA determinization"
    This reverts commit e7c8589fd8170e55bcd6fd3f887a56c9ccb702b3.

--- src/precompute4/weighted_automata/tuple_merger.rs
+++ src/precompute4/weighted_automata/tuple_merger.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use std::collections::{BTreeMap, HashMap, VecDeque};
+use std::collections::{BTreeMap, BTreeSet, HashMap, VecDeque};
 
 pub type ProductTuple = Vec<Option<usize>>;
 
@@ -50,6 +50,69 @@ pub fn successor_tuple(
     out
 }
 
+pub fn merge_and_build_automaton(
+    start_tuple: ProductTuple,
+    components: &[Vec<BTreeMap<usize, usize>>],
+    other_index: usize,
+) -> (Vec<ProductTuple>, HashMap<ProductTuple, usize>) {
+    let mut states: Vec<ProductTuple> = Vec::new();
+    let mut point_map: HashMap<ProductTuple, usize> = HashMap::new();
+    let mut worklist: VecDeque<usize> = VecDeque::new();
+
+    let start_id = 0;
+    states.push(start_tuple.clone());
+    point_map.insert(start_tuple, start_id);
+    worklist.push_back(start_id);
+
+    while let Some(state_id) = worklist.pop_front() {
+        let representative = states[state_id].clone();
+
+        let mut alphabet = BTreeSet::new();
+        for (i, comp_state_opt) in representative.iter().enumerate() {
+            if let Some(comp_state) = comp_state_opt {
+                for &symbol in components[i][*comp_state].keys() {
+                    alphabet.insert(symbol);
+                }
+            }
+        }
+        alphabet.insert(other_index);
+
+        for symbol in alphabet {
+
+            let successor = successor_tuple(&representative, symbol, components);
+
+            if point_map.contains_key(&successor) {
+                continue;
+            }
+
+            let mut assigned_id = None;
+            for id in 0..states.len() {
+                if let Some(new_rep) = unify_tuples(&states[id], &successor) {
+                    if new_rep != states[id] {
+                        states[id] = new_rep;
+                        if !worklist.contains(&id) {
+                            worklist.push_back(id);
+                        }
+                    }
+                    assigned_id = Some(id);
+                    break;
+                }
+            }
+
+            let home_id = assigned_id.unwrap_or_else(|| {
+                let new_id = states.len();
+                states.push(successor.clone());
+                worklist.push_back(new_id);
+                new_id
+            });
+
+            point_map.insert(successor, home_id);
+        }
+    }
+
+    (states, point_map)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -61,5 +124,25 @@ mod tests {
         assert_eq!(unify_tuples(&vec![Some(1), None], &vec![Some(1), Some(3)]), Some(vec![Some(1), Some(3)]));
         assert_eq!(unify_tuples(&vec![Some(1), Some(2)], &vec![Some(1), Some(3)]), None);
         assert_eq!(unify_tuples(&vec![None, None], &vec![Some(1), Some(2)]), Some(vec![Some(1), Some(2)]));
+    }
+
+    #[test]
+    fn test_simple_merge() {
+        let comp0 = vec![BTreeMap::from([(0, 0)])];
+        let comp1 = vec![BTreeMap::from([(1, 0)])];
+        let components = vec![comp0, comp1];
+        let other_index = 2;
+
+        let start_tuple = vec![Some(0), Some(0)];
+
+        let (states, point_map) = merge_and_build_automaton(start_tuple, &components, other_index);
+
+        assert_eq!(states.len(), 1);
+        assert_eq!(states[0], vec![Some(0), Some(0)]);
+
+        let succ0 = successor_tuple(&states[0], 0, &components);
+        let succ1 = successor_tuple(&states[0], 1, &components);
+        assert_eq!(*point_map.get(&succ0).unwrap(), 0);
+        assert_eq!(*point_map.get(&succ1).unwrap(), 0);
     }
 }
