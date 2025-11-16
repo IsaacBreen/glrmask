8a43218f8569c97b59342a2a49cfdd5be2142f1a
IsaacBreen <mail@isaacbreen.com>
2025-11-16 15:00:28 +0800

    Add test case for negative code resolution on complex NWA structure from log

--- src/precompute4/test_resolve_negatives.rs
+++ src/precompute4/test_resolve_negatives.rs
@@ -33,6 +33,273 @@ fn test_resolve_negatives_simple_cancellation() {
 }
 
 #[test]
+fn test_resolve_negatives_from_large_nwa_log() {
+    use crate::precompute4::resolve_negatives::resolve_negative_codes_in_nwa;
+    let mut nwa = NWA::new();
+    let mut states = Vec::new();
+    for _ in 0..69 {
+        states.push(nwa.add_state());
+    }
+    nwa.start_state = states[68];
+
+    // State 0:
+    nwa.states[states[0]].final_weight = Some(Weight::all());
+    // State 1:
+    nwa.add_epsilon(states[1], states[0], Weight::all());
+    // State 2:
+    nwa.add_epsilon(states[2], states[1], Weight::all());
+    // State 3:
+    nwa.add_epsilon(states[3], states[4], Weight::all());
+    // State 4:
+    nwa.add_epsilon(states[4], states[5], Weight::all());
+    // State 5:
+    nwa.add_transition(states[5], 0, states[6], Weight::all());
+    nwa.add_transition(states[5], 69, states[6], Weight::all());
+    nwa.add_transition(states[5], 79, states[6], Weight::all());
+    nwa.add_transition(states[5], 101, states[6], Weight::all());
+    nwa.add_transition(states[5], 131, states[6], Weight::all());
+    nwa.add_transition(states[5], 151, states[6], Weight::all());
+    nwa.add_transition(states[5], 161, states[6], Weight::all());
+    nwa.add_transition(states[5], 165, states[6], Weight::all());
+    nwa.add_transition(states[5], 166, states[6], Weight::all());
+    nwa.add_transition(states[5], 279, states[6], Weight::all());
+    nwa.add_transition(states[5], 280, states[6], Weight::all());
+    nwa.add_transition(states[5], 286, states[6], Weight::all());
+    nwa.add_transition(states[5], 300, states[6], Weight::all());
+    nwa.add_transition(states[5], 310, states[6], Weight::all());
+    nwa.add_transition(states[5], 371, states[6], Weight::all());
+    nwa.add_transition(states[5], 400, states[6], Weight::all());
+    nwa.add_transition(states[5], 422, states[8], Weight::all());
+    nwa.add_transition(states[5], 429, states[6], Weight::all());
+    nwa.add_transition(states[5], 436, states[9], Weight::all());
+    nwa.add_transition(states[5], 437, states[10], Weight::all());
+    nwa.add_transition(states[5], 438, states[11], Weight::all());
+    nwa.add_transition(states[5], 458, states[12], Weight::all());
+    nwa.add_transition(states[5], 459, states[7], Weight::all());
+    nwa.add_transition(states[5], 476, states[6], Weight::all());
+    // State 6:
+    nwa.add_transition(states[6], 422, states[13], Weight::all());
+    nwa.add_transition(states[6], 436, states[14], Weight::all());
+    nwa.add_transition(states[6], 437, states[11], Weight::all());
+    // State 7:
+    nwa.add_transition(states[7], 32767, states[6], Weight::all());
+    // State 8:
+    nwa.add_transition(states[8], i16::MIN + 422, states[15], Weight::all());
+    // State 9:
+    nwa.add_transition(states[9], i16::MIN + 436, states[15], Weight::all());
+    // State 10:
+    nwa.add_transition(states[10], i16::MIN + 437, states[15], Weight::all());
+    // State 11:
+    nwa.add_transition(states[11], 436, states[14], Weight::all());
+    // State 12:
+    nwa.add_transition(states[12], i16::MIN + 458, states[16], Weight::all());
+    // State 13:
+    nwa.add_transition(states[13], i16::MIN + 422, states[9], Weight::all());
+    // State 14:
+    nwa.add_transition(states[14], i16::MIN + 436, states[10], Weight::all());
+    // State 15:
+    nwa.add_transition(states[15], i16::MIN + 458, states[17], Weight::all());
+    // State 16:
+    nwa.add_transition(states[16], i16::MIN + 459, states[17], Weight::all());
+    // State 17:
+    nwa.add_epsilon(states[17], states[2], Weight::from_item(1));
+    // State 18:
+    nwa.add_epsilon(states[18], states[3], Weight::all());
+    // State 19:
+    nwa.add_epsilon(states[19], states[2], Weight::from_ranges(&[(0, 5)]).unwrap());
+    // State 20:
+    nwa.add_epsilon(states[20], states[19], Weight::all());
+    // State 21:
+    nwa.add_epsilon(states[21], states[22], Weight::all());
+    // State 22:
+    nwa.add_transition(states[22], 0, states[23], Weight::all());
+    nwa.add_transition(states[22], 69, states[23], Weight::all());
+    nwa.add_transition(states[22], 79, states[23], Weight::all());
+    nwa.add_transition(states[22], 101, states[23], Weight::all());
+    nwa.add_transition(states[22], 131, states[23], Weight::all());
+    nwa.add_transition(states[22], 151, states[23], Weight::all());
+    nwa.add_transition(states[22], 161, states[23], Weight::all());
+    nwa.add_transition(states[22], 165, states[23], Weight::all());
+    nwa.add_transition(states[22], 166, states[23], Weight::all());
+    nwa.add_transition(states[22], 279, states[23], Weight::all());
+    nwa.add_transition(states[22], 280, states[23], Weight::all());
+    nwa.add_transition(states[22], 286, states[23], Weight::all());
+    nwa.add_transition(states[22], 300, states[23], Weight::all());
+    nwa.add_transition(states[22], 310, states[23], Weight::all());
+    nwa.add_transition(states[22], 371, states[23], Weight::all());
+    nwa.add_transition(states[22], 400, states[23], Weight::all());
+    nwa.add_transition(states[22], 422, states[25], Weight::all());
+    nwa.add_transition(states[22], 429, states[23], Weight::all());
+    nwa.add_transition(states[22], 436, states[26], Weight::all());
+    nwa.add_transition(states[22], 437, states[27], Weight::all());
+    nwa.add_transition(states[22], 438, states[28], Weight::all());
+    nwa.add_transition(states[22], 458, states[29], Weight::all());
+    nwa.add_transition(states[22], 459, states[24], Weight::all());
+    nwa.add_transition(states[22], 476, states[23], Weight::all());
+    // State 23:
+    nwa.add_transition(states[23], 422, states[30], Weight::all());
+    nwa.add_transition(states[23], 436, states[31], Weight::all());
+    nwa.add_transition(states[23], 437, states[28], Weight::all());
+    // State 24:
+    nwa.add_transition(states[24], 32767, states[23], Weight::all());
+    // State 25:
+    nwa.add_transition(states[25], i16::MIN + 422, states[32], Weight::all());
+    // State 26:
+    nwa.add_transition(states[26], i16::MIN + 436, states[32], Weight::all());
+    // State 27:
+    nwa.add_transition(states[27], i16::MIN + 437, states[32], Weight::all());
+    // State 28:
+    nwa.add_transition(states[28], 436, states[31], Weight::all());
+    // State 29:
+    nwa.add_transition(states[29], i16::MIN + 458, states[33], Weight::all());
+    // State 30:
+    nwa.add_transition(states[30], i16::MIN + 422, states[26], Weight::all());
+    // State 31:
+    nwa.add_transition(states[31], i16::MIN + 436, states[27], Weight::all());
+    // State 32:
+    nwa.add_transition(states[32], i16::MIN + 458, states[34], Weight::all());
+    // State 33:
+    nwa.add_transition(states[33], i16::MIN + 459, states[34], Weight::all());
+    // State 34:
+    nwa.add_epsilon(states[34], states[18], Weight::from_item(1));
+    // State 35:
+    nwa.add_epsilon(states[35], states[21], Weight::all());
+    // State 36:
+    nwa.add_epsilon(states[36], states[37], Weight::all());
+    // State 37:
+    nwa.add_epsilon(states[37], states[38], Weight::all());
+    // State 38:
+    nwa.add_transition(states[38], 0, states[39], Weight::all());
+    nwa.add_transition(states[38], 69, states[39], Weight::all());
+    nwa.add_transition(states[38], 79, states[39], Weight::all());
+    nwa.add_transition(states[38], 101, states[39], Weight::all());
+    nwa.add_transition(states[38], 131, states[39], Weight::all());
+    nwa.add_transition(states[38], 151, states[39], Weight::all());
+    nwa.add_transition(states[38], 161, states[39], Weight::all());
+    nwa.add_transition(states[38], 165, states[39], Weight::all());
+    nwa.add_transition(states[38], 166, states[39], Weight::all());
+    nwa.add_transition(states[38], 279, states[39], Weight::all());
+    nwa.add_transition(states[38], 280, states[39], Weight::all());
+    nwa.add_transition(states[38], 286, states[39], Weight::all());
+    nwa.add_transition(states[38], 300, states[39], Weight::all());
+    nwa.add_transition(states[38], 310, states[39], Weight::all());
+    nwa.add_transition(states[38], 371, states[39], Weight::all());
+    nwa.add_transition(states[38], 400, states[39], Weight::all());
+    nwa.add_transition(states[38], 422, states[41], Weight::all());
+    nwa.add_transition(states[38], 429, states[39], Weight::all());
+    nwa.add_transition(states[38], 436, states[42], Weight::all());
+    nwa.add_transition(states[38], 437, states[43], Weight::all());
+    nwa.add_transition(states[38], 438, states[44], Weight::all());
+    nwa.add_transition(states[38], 458, states[45], Weight::all());
+    nwa.add_transition(states[38], 459, states[40], Weight::all());
+    nwa.add_transition(states[38], 476, states[39], Weight::all());
+    // State 39:
+    nwa.add_transition(states[39], 422, states[46], Weight::all());
+    nwa.add_transition(states[39], 436, states[47], Weight::all());
+    nwa.add_transition(states[39], 437, states[44], Weight::all());
+    // State 40:
+    nwa.add_transition(states[40], 32767, states[39], Weight::all());
+    // State 41:
+    nwa.add_transition(states[41], i16::MIN + 422, states[48], Weight::all());
+    // State 42:
+    nwa.add_transition(states[42], i16::MIN + 436, states[48], Weight::all());
+    // State 43:
+    nwa.add_transition(states[43], i16::MIN + 437, states[48], Weight::all());
+    // State 44:
+    nwa.add_transition(states[44], 436, states[47], Weight::all());
+    // State 45:
+    nwa.add_transition(states[45], i16::MIN + 458, states[49], Weight::all());
+    // State 46:
+    nwa.add_transition(states[46], i16::MIN + 422, states[42], Weight::all());
+    // State 47:
+    nwa.add_transition(states[47], i16::MIN + 436, states[43], Weight::all());
+    // State 48:
+    nwa.add_transition(states[48], i16::MIN + 458, states[50], Weight::all());
+    // State 49:
+    nwa.add_transition(states[49], i16::MIN + 459, states[50], Weight::all());
+    // State 50:
+    nwa.add_epsilon(states[50], states[35], Weight::from_item(1));
+    // State 51:
+    nwa.add_epsilon(states[51], states[36], Weight::all());
+    // State 52:
+    nwa.add_epsilon(states[52], states[53], Weight::all());
+    nwa.add_epsilon(states[52], states[54], Weight::all());
+    // State 53:
+    nwa.add_epsilon(states[53], states[51], Weight::from_ranges(&[(2, 2), (4, 5)]).unwrap());
+    // State 54:
+    nwa.add_transition(states[54], 0, states[55], Weight::all());
+    nwa.add_transition(states[54], 69, states[55], Weight::all());
+    nwa.add_transition(states[54], 79, states[55], Weight::all());
+    nwa.add_transition(states[54], 101, states[55], Weight::all());
+    nwa.add_transition(states[54], 131, states[55], Weight::all());
+    nwa.add_transition(states[54], 151, states[55], Weight::all());
+    nwa.add_transition(states[54], 161, states[55], Weight::all());
+    nwa.add_transition(states[54], 165, states[55], Weight::all());
+    nwa.add_transition(states[54], 166, states[55], Weight::all());
+    nwa.add_transition(states[54], 279, states[55], Weight::all());
+    nwa.add_transition(states[54], 280, states[55], Weight::all());
+    nwa.add_transition(states[54], 286, states[55], Weight::all());
+    nwa.add_transition(states[54], 300, states[55], Weight::all());
+    nwa.add_transition(states[54], 310, states[55], Weight::all());
+    nwa.add_transition(states[54], 371, states[55], Weight::all());
+    nwa.add_transition(states[54], 400, states[55], Weight::all());
+    nwa.add_transition(states[54], 422, states[57], Weight::all());
+    nwa.add_transition(states[54], 429, states[55], Weight::all());
+    nwa.add_transition(states[54], 436, states[58], Weight::all());
+    nwa.add_transition(states[54], 437, states[59], Weight::all());
+    nwa.add_transition(states[54], 438, states[60], Weight::all());
+    nwa.add_transition(states[54], 458, states[61], Weight::all());
+    nwa.add_transition(states[54], 459, states[56], Weight::all());
+    nwa.add_transition(states[54], 476, states[55], Weight::all());
+    // State 55:
+    nwa.add_transition(states[55], 422, states[62], Weight::all());
+    nwa.add_transition(states[55], 436, states[63], Weight::all());
+    nwa.add_transition(states[55], 437, states[60], Weight::all());
+    // State 56:
+    nwa.add_transition(states[56], 32767, states[55], Weight::all());
+    // State 57:
+    nwa.add_transition(states[57], i16::MIN + 422, states[64], Weight::all());
+    // State 58:
+    nwa.add_transition(states[58], i16::MIN + 436, states[64], Weight::all());
+    // State 59:
+    nwa.add_transition(states[59], i16::MIN + 437, states[64], Weight::all());
+    // State 60:
+    nwa.add_transition(states[60], 436, states[63], Weight::all());
+    // State 61:
+    nwa.add_transition(states[61], i16::MIN + 458, states[65], Weight::all());
+    // State 62:
+    nwa.add_transition(states[62], i16::MIN + 422, states[58], Weight::all());
+    // State 63:
+    nwa.add_transition(states[63], i16::MIN + 436, states[59], Weight::all());
+    // State 64:
+    nwa.add_transition(states[64], i16::MIN + 458, states[66], Weight::all());
+    // State 65:
+    nwa.add_transition(states[65], i16::MIN + 459, states[66], Weight::all());
+    // State 66:
+    nwa.add_epsilon(states[66], states[51], Weight::from_item(1));
+    // State 67:
+    nwa.add_epsilon(states[67], states[52], Weight::all());
+    // State 68:
+    nwa.add_transition(states[68], 0, states[67], Weight::all());
+    nwa.add_transition(states[68], 161, states[20], Weight::all());
+    nwa.add_transition(states[68], 165, states[20], Weight::all());
+    nwa.add_transition(states[68], 166, states[20], Weight::all());
+
+    resolve_negative_codes_in_nwa(&mut nwa);
+    let mut d = nwa.determinize_to_dwa();
+    d.simplify();
+
+    let mut expected = DWA::new();
+    let s_final = expected.add_state();
+    expected.set_final_weight(s_final, Weight::from_ranges(&[(0, 5)]).unwrap()).unwrap();
+    expected.add_transition(expected.body.start_state, 161, s_final, Weight::all()).unwrap();
+    expected.add_transition(expected.body.start_state, 165, s_final, Weight::all()).unwrap();
+    expected.add_transition(expected.body.start_state, 166, s_final, Weight::all()).unwrap();
+
+    stochastic_equivalence_test(d, expected);
+}
+
+#[test]
 fn test_resolve_negatives_from_nwa_log_2() {
     let mut d = DWA::new();
     let mut states = vec![d.body.start_state];
