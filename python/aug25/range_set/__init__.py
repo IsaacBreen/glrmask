8bdc081b8bdf0d4915f1bd64b24b911d0b44e5ee
IsaacBreen <mail@isaacbreen.com>
2025-11-17 18:55:02 +0800

    Feat: Implement BitsetRangeSet wrapper

--- python/aug25/range_set/__init__.py
+++ python/aug25/range_set/__init__.py
@@ -1,11 +1,11 @@
 from .py_range_set import PyRangeSet
 from .ffi_range_set import FFIRangeSet
-from .bitset_range_set import BitsetRangeSet
 from .roaring_range_set import RoaringRangeSet
 from .set_range_set import SetRangeSet
+from .bitset_range_set import BitsetRangeSet
 
 # RangeSet = PyRangeSet
 RangeSet = FFIRangeSet
 
-__all__ = ["RangeSet", "PyRangeSet", "FFIRangeSet", "BitsetRangeSet", "RoaringRangeSet", "SetRangeSet"]
+__all__ = ["RangeSet", "PyRangeSet", "FFIRangeSet", "RoaringRangeSet", "SetRangeSet", "BitsetRangeSet"]
 
