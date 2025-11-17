94150757bf61f1fd9f35ce3e000b635c8fafa666
IsaacBreen <mail@isaacbreen.com>
2025-11-17 10:12:32 +0800

    NICE!!! working. re-enable

--- src/constraint_precompute1_utils.rs
+++ src/constraint_precompute1_utils.rs
@@ -41,7 +41,7 @@ impl Default for Trie1Config {
     fn default() -> Self {
         Self {
             enabled: true,
-            break_cycles: false,
+            break_cycles: true,
             early_flatten_epsilon: false,
             minimize_by_signature: true,
             merge_equivalent_llm_tokens: true,
