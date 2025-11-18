50d7c60443813d5778df747b7e36f012bcbe7c5b
IsaacBreen <mail@isaacbreen.com>
2025-11-18 21:10:24 +0800

    Optimize table generation: use NT IDs in analysis and introduce default actions for lookahead

--- src/glr/parser.rs
+++ src/glr/parser.rs
@@ -377,7 +377,10 @@ impl GLRParser {
             };
             let action = match row.shifts_and_reduces_full.get(&token) {
                 Some(a) => a,
-                None => continue,
+                None => match &row.default_action {
+                    Some(da) => da,
+                    None => continue,
+                },
             };
 
             match action {
