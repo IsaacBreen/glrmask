517349dab308ca419a47897117990a3a7cafa430
IsaacBreen <mail@isaacbreen.com>
2025-11-19 22:36:16 +0800

    Optimize DWA cycle unrolling by improving visited tracking and transition creation

--- src/precompute4/weighted_automata/unroll.rs
+++ src/precompute4/weighted_automata/unroll.rs
@@ -24,22 +24,28 @@ impl DWA {
             return new_dwa;
         }
 
-        // Map (original_state, accumulated_weight) -> new_state_id
-        let mut visited: HashMap<(StateID, Weight), StateID> = HashMap::new();
-        let mut queue: VecDeque<(StateID, Weight)> = VecDeque::new();
+        // Optimization: Use a vector of maps instead of a single map.
+        // visited[original_state_id] -> HashMap<Weight, new_state_id>
+        let mut visited: Vec<HashMap<Weight, StateID>> = vec![HashMap::new(); self.states.len()];
+        
+        // Queue stores (new_state_id, original_state_id, accumulated_weight)
+        let mut queue: VecDeque<(StateID, StateID, Weight)> = VecDeque::new();
 
         let new_start = new_dwa.add_state();
         new_dwa.body.start_state = new_start;
 
         // Copy properties for start state
-        new_dwa.states[new_start].state_weight = self.states[start_node].state_weight.clone();
-        new_dwa.states[new_start].final_weight = self.states[start_node].final_weight.clone();
+        {
+            let start_state_ref = &self.states[start_node];
+            let new_state_ref = &mut new_dwa.states[new_start];
+            new_state_ref.state_weight = start_state_ref.state_weight.clone();
+            new_state_ref.final_weight = start_state_ref.final_weight.clone();
+        }
 
-        visited.insert((start_node, start_weight.clone()), new_start);
-        queue.push_back((start_node, start_weight));
+        visited[start_node].insert(start_weight.clone(), new_start);
+        queue.push_back((new_start, start_node, start_weight));
 
-        while let Some((u, w_u)) = queue.pop_front() {
-            let new_u = *visited.get(&(u, w_u.clone())).unwrap();
+        while let Some((new_u, u, w_u)) = queue.pop_front() {
             let u_state = &self.states[u];
 
             for (&label, &v) in &u_state.transitions {
@@ -48,30 +54,36 @@ impl DWA {
                 }
 
                 let trans_w = u_state.trans_weights.get(&label).unwrap();
-                let mut next_w = w_u.clone();
-                next_w &= trans_w;
+                let mut next_w = &w_u & trans_w;
+                if next_w.is_empty() {
+                    continue;
+                }
 
                 let v_state = &self.states[v];
                 if let Some(sw) = &v_state.state_weight {
                     next_w &= sw;
+                    if next_w.is_empty() {
+                        continue;
+                    }
                 }
 
-                if !next_w.is_empty() {
-                    let next_key = (v, next_w.clone());
+                let v_visited = &mut visited[v];
+                let new_v = if let Some(&id) = v_visited.get(&next_w) {
+                    id
+                } else {
+                    let id = new_dwa.add_state();
+                    let new_st = &mut new_dwa.states[id];
+                    new_st.state_weight = v_state.state_weight.clone();
+                    new_st.final_weight = v_state.final_weight.clone();
+                    
+                    v_visited.insert(next_w.clone(), id);
+                    queue.push_back((id, v, next_w));
+                    id
+                };
 
-                    let new_v = if let Some(&id) = visited.get(&next_key) {
-                        id
-                    } else {
-                        let id = new_dwa.add_state();
-                        new_dwa.states[id].state_weight = v_state.state_weight.clone();
-                        new_dwa.states[id].final_weight = v_state.final_weight.clone();
-                        visited.insert(next_key.clone(), id);
-                        queue.push_back(next_key);
-                        id
-                    };
-
-                    let _ = new_dwa.add_transition(new_u, label, new_v, trans_w.clone());
-                }
+                let src_st = &mut new_dwa.states[new_u];
+                src_st.transitions.insert(label, new_v);
+                src_st.trans_weights.insert(label, trans_w.clone());
             }
         }
 
