f06ff782a4a5d1a4b10970041530f1225320af0b
IsaacBreen <mail@isaacbreen.com>
2025-11-25 15:53:22 +0800

    ok

--- src/bin/grammar_compiler.rs
+++ src/bin/grammar_compiler.rs
@@ -42,7 +42,7 @@ struct Args {
 }
 
 fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let mut instant = std::time::Instant::now();
+    let instant = std::time::Instant::now();
     let args = Args::parse();
 
     if !args.precompute0_only && args.output.is_none() {
