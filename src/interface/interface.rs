5910363debaeef779b2c11ff4a546f0894bec168
IsaacBreen <mail@isaacbreen.com>
2025-12-02 14:31:59 +0800

    Revert "fix: disable automatic grammar optimization in from_exprs"
    This reverts commit 667bfee6777aa774512f19192e9fadca93339a65.

--- src/interface/interface.rs
+++ src/interface/interface.rs
@@ -1170,11 +1170,7 @@ impl GrammarDefinition {
             external_name_to_group_id: BiBTreeMap::new(),
         };
 
-        // NOTE: Optimization is disabled by default because it can convert CFG grammars
-        // to regular expressions in cases where the CFG structure is needed for correct
-        // parsing behavior (e.g., grammars with optional elements that require lookahead).
-        // Call def.optimize() explicitly if you need terminal optimization.
-        // def.optimize();
+        def.optimize();
 
         Ok(def)
     }
