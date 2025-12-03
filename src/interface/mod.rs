0ee995bb2b9406018e2e602191e475fdb967d57a
IsaacBreen <mail@isaacbreen.com>
2025-12-03 14:00:09 +0800

    Revert "attempt"
    This reverts commit 37cbf1e7f002d1b38725491da9352ca1609ecd5d.

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -3,7 +3,7 @@ mod tokenizer_combinators;
 mod tests;
 mod ebnf;
 mod lark;
-pub mod optimization;
+mod optimization;
 
 pub use ebnf::*;
 pub use interface::{choice, display_productions, literal, optional, r#ref, repeat, sequence, CompiledGrammar, GrammarDefinition, GrammarExpr, IncrementalParser, ExprNullability, get_expr_nullability};
