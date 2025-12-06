cc3cd5fe18c933b8d29aa7d2bb8dddf4044e28bc
IsaacBreen <mail@isaacbreen.com>
2025-12-06 16:56:18 +0800

    ok! nice

---
+++ src/equivalence_analysis.rs
@@ -0,0 +1,24 @@
+use crate::finite_automata::Regex;
+use hashbrown::{HashMap, HashSet};
+use rayon::prelude::*;
+use std::collections::{BTreeSet, VecDeque};
+use std::hash::{Hash, Hasher};
+use std::collections::hash_map::DefaultHasher;
+use crate::{equivalence_analysis_fast, equivalence_analysis_reference};
+
+pub type EquivalenceResult = BTreeSet<Vec<usize>>;
+
+pub fn find_equivalence_classes(
+    regex: &Regex,
+    strings: &[Vec<u8>],
+    initial_states: &[usize],
+) -> EquivalenceResult {
+    let instant = std::time::Instant::now();
+    let reference = equivalence_analysis_reference::find_equivalence_classes(regex, strings, initial_states);
+    crate::debug!(3, "Reference equivalence analysis took {:?}", instant.elapsed());
+    let instant = std::time::Instant::now();
+    let fast = equivalence_analysis_fast::find_equivalence_classes(regex, strings, initial_states);
+    crate::debug!(3, "Fast equivalence analysis took {:?}", instant.elapsed());
+    assert_eq!(reference, fast, "Mismatch between reference and fast equivalence analysis results");
+    reference
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![allow(warnings)]
 pub mod finite_automata;
+mod equivalence_analysis_reference;
+mod equivalence_analysis_fast;
 mod equivalence_analysis;
 pub mod glr;
 pub mod constraint;
