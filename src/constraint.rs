42737bd7e6b633864d90f3a0b3f9cbe0fd460452
IsaacBreen <mail@isaacbreen.com>
2026-01-15 18:16:16 +0800

    chore: remove unused weight metrics modules

--- src/constraint.rs
+++ src/constraint.rs
@@ -1246,10 +1246,6 @@ impl GrammarConstraint {
                 terminal_dwa.num_ranges_interned(),
                 terminal_dwa.num_ranges(),
             );
-            crate::dwa_i32::weight_bdd_metrics::maybe_print_dwa_weight_bdd_metrics(&terminal_dwa, domain_max, "Terminal DWA");
-            crate::dwa_i32::weight_oxidd_metrics::maybe_print_dwa_weight_oxidd_metrics(&terminal_dwa, domain_max, "Terminal DWA");
-            crate::dwa_i32::weight_factorization_metrics::maybe_print_dwa_weight_factorization_metrics(&terminal_dwa, "Terminal DWA");
-            crate::dwa_i32::weight_factorization_metrics::maybe_print_2d_factorization_metrics(&terminal_dwa, vocab.internal_max_llm_token + 1, num_tsids, "Terminal DWA");
         }
         
         if crate::r#macro::is_debug_level_enabled(4) {
@@ -2244,10 +2240,6 @@ impl GrammarConstraint {
                 parser_dwa.num_ranges_interned(),
                 parser_dwa.num_ranges(),
             );
-            crate::dwa_i32::weight_bdd_metrics::maybe_print_dwa_weight_bdd_metrics(&parser_dwa, domain_max, "Parser DWA");
-            crate::dwa_i32::weight_oxidd_metrics::maybe_print_dwa_weight_oxidd_metrics(&parser_dwa, domain_max, "Parser DWA");
-            crate::dwa_i32::weight_factorization_metrics::maybe_print_dwa_weight_factorization_metrics(&parser_dwa, "Parser DWA");
-            crate::dwa_i32::weight_factorization_metrics::maybe_print_2d_factorization_metrics(&parser_dwa, vocab.internal_max_llm_token + 1, num_tsids, "Parser DWA");
         }
 
         let internal_to_original_sparse_matrix =
