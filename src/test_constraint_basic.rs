136b085249056931270f32756256521158f38116
IsaacBreen <mail@isaacbreen.com>
2026-02-26 15:17:33 +0800

    simpler!

--- src/test_constraint_basic.rs
+++ src/test_constraint_basic.rs
@@ -6722,10 +6722,9 @@ fn test_github_easy_o63377_false_positive_a() {
     let _guard = crate::GLOBAL_DIMS_MUTEX.lock().unwrap_or_else(|e| e.into_inner());
 
     let lark = indoc! {r#"
-        A: "a" "b"*
+        A: "a" ("b"?)
         B: "ac"
-        C: [ab]*
-        start: A B C
+        start: A B
     "#};
     let grammar_definition = GrammarDefinition::from_lark(lark).expect("from_lark");
 
