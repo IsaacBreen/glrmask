3fce3c948001e439399b71deaaea572272845bcd
IsaacBreen <mail@isaacbreen.com>
2026-03-12 11:34:15 +0800

    improvements

--- src/compiler/stages/terminal_dwa.rs
+++ src/compiler/stages/terminal_dwa.rs
@@ -615,8 +615,18 @@ impl<'tok, 'pm, 'nwa> TerminalNwaBuilder<'tok, 'pm, 'nwa> {
     }
 
     fn buffer_leaf_token_id_set(&mut self, source: u32, label: TerminalID, token_ids: &RangeSetBlaze<usize>) {
+        let source_idx = source as usize;
+        if source_idx >= self.leaf_token_ids_buffer.len() {
+            self.leaf_token_ids_buffer.resize_with(source_idx + 1, Vec::new);
+        }
+        let labels = &mut self.leaf_token_ids_buffer[source_idx];
+        let label_idx = label as usize;
+        if label_idx >= labels.len() {
+            labels.resize_with(label_idx + 1, Vec::new);
+        }
         for token_id in token_ids.iter() {
-            self.buffer_leaf_token_id(source, label, token_id as u32);
+            let internal_token_id = token_id as u32;
+            labels[label_idx].push(internal_token_id);
         }
     }
 
@@ -670,7 +680,16 @@ impl<'tok, 'pm, 'nwa> TerminalNwaBuilder<'tok, 'pm, 'nwa> {
         }
 
         for &source in sources {
-            self.buffer_leaf_token_id(source, label, internal_token_id);
+            let source_idx = source as usize;
+            if source_idx >= self.leaf_token_ids_buffer.len() {
+                self.leaf_token_ids_buffer.resize_with(source_idx + 1, Vec::new);
+            }
+            let labels = &mut self.leaf_token_ids_buffer[source_idx];
+            let label_idx = label as usize;
+            if label_idx >= labels.len() {
+                labels.resize_with(label_idx + 1, Vec::new);
+            }
+            labels[label_idx].push(internal_token_id);
         }
     }
 
