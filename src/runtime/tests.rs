c50c4d4a21aac9ec852ff94205f3e887e0eef575
IsaacBreen <mail@isaacbreen.com>
2026-03-19 13:19:09 +0800

    ok! fails LMAO

--- src/runtime/tests.rs
+++ src/runtime/tests.rs
@@ -17,20 +17,11 @@ fn token_allowed(mask: &[u32], id: usize) -> bool {
 
 #[test]
 fn test_simple_ab() {
-    let vocab = make_vocab(&["a", "b", "ab"]);
-    let c = Constraint::from_ebnf(
-        r#"
-        start ::= AB
-        AB ::= 'a' 'b'
-        "#,
-        &vocab,
-    )
-        .unwrap();
+    let vocab = make_vocab(&["a"]);
+    let c = Constraint::from_ebnf("start ::= 'a' 'b'", &vocab).unwrap();
 
     let mut s = c.start();
     s.commit_bytes(b"a");
     let mask = s.mask();
     assert!(!token_allowed(&mask, 0), "token 'a' should NOT be allowed");
-    assert!(token_allowed(&mask, 1), "token 'b' should be allowed");
-    assert!(!token_allowed(&mask, 2), "token 'ab' should NOT be allowed");
 }
