0fdfaba1c606a35d0ec7e5186c3b097a16f6ae2d
IsaacBreen <mail@isaacbreen.com>
2026-05-26 17:14:15 +0800

    Support IPvFuture URI literals

--- src/import/json_schema/tests.rs
+++ src/import/json_schema/tests.rs
@@ -1197,6 +1197,7 @@ fn uri_string_value_satisfaction_filters_invalid_literals() {
 
     assert!(string_value_satisfies_schema(&json!("ecdsa-koblitz-pubkey:abc123"), &schema).unwrap());
     assert!(string_value_satisfies_schema(&json!("ecdsa-koblitz-pubkey://[::1]"), &schema).unwrap());
+    assert!(string_value_satisfies_schema(&json!("ftp://[v1.example]"), &schema).unwrap());
     assert!(!string_value_satisfies_schema(&json!("<<"), &schema).unwrap());
     assert!(!string_value_satisfies_schema(&json!("ecd:]"), &schema).unwrap());
     assert!(!string_value_satisfies_schema(&json!("ecd://["), &schema).unwrap());
