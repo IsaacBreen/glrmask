0415592dc53077acca8b57eb056d4b651b7442f6
IsaacBreen <mail@isaacbreen.com>
2026-06-08 20:32:17 +0800

    Revert "minimize"
    This reverts commit b974cf68bc4c907c74be658af23055d8f6b25be3.

--- tests/unicode_escape_progression_glrm_mre.rs
+++ tests/unicode_escape_progression_glrm_mre.rs
@@ -9,7 +9,7 @@ fn token_allowed(mask: &[u32], id: usize) -> bool {
 const GLRM_UNICODE_ESCAPE_PROGRESS_MRE: &str = r#"
 start s;
 
-nt s ::= esc "\"";
+nt s ::= "\"" esc "\"";
 t esc ::= /\\u00(?:[01][0-9A-Fa-f]|7[Ff])/;
 "#;
 
@@ -18,6 +18,7 @@ fn glrm_unicode_escape_progression_allows_bare_u() {
     let vocab = Vocab::new(vec![(0u32, br#"\u"#.to_vec())], None);
     let constraint = Constraint::from_glrm_grammar(GLRM_UNICODE_ESCAPE_PROGRESS_MRE, &vocab).unwrap();
     let mut state = constraint.start();
+    state.commit_bytes(b"\"").unwrap();
 
     let mask = state.mask();
     assert!(token_allowed(&mask, 0), r#"bare \u should be live because \u00.. is valid"#);
@@ -28,6 +29,7 @@ fn glrm_unicode_escape_progression_rejects_u_c() {
     let vocab = Vocab::new(vec![(0u32, br#"\uC"#.to_vec())], None);
     let constraint = Constraint::from_glrm_grammar(GLRM_UNICODE_ESCAPE_PROGRESS_MRE, &vocab).unwrap();
     let mut state = constraint.start();
+    state.commit_bytes(b"\"").unwrap();
 
     let mask = state.mask();
     assert!(!token_allowed(&mask, 0), r#"\uC should already be dead for /\\u00.../"#);
@@ -38,6 +40,7 @@ fn glrm_unicode_escape_progression_allows_zero_after_bare_u() {
     let vocab = Vocab::new(vec![(0u32, br#"\u"#.to_vec()), (1u32, b"0".to_vec())], None);
     let constraint = Constraint::from_glrm_grammar(GLRM_UNICODE_ESCAPE_PROGRESS_MRE, &vocab).unwrap();
     let mut state = constraint.start();
+    state.commit_bytes(b"\"").unwrap();
     state.commit_bytes(br#"\u"#).unwrap();
 
     let mask = state.mask();
@@ -49,6 +52,7 @@ fn glrm_unicode_escape_progression_rejects_c_after_bare_u() {
     let vocab = Vocab::new(vec![(0u32, br#"\u"#.to_vec()), (1u32, b"C".to_vec())], None);
     let constraint = Constraint::from_glrm_grammar(GLRM_UNICODE_ESCAPE_PROGRESS_MRE, &vocab).unwrap();
     let mut state = constraint.start();
+    state.commit_bytes(b"\"").unwrap();
     state.commit_bytes(br#"\u"#).unwrap();
 
     let mask = state.mask();
